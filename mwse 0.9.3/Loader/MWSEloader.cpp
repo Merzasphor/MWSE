d431abb84484d2f152930176cc0c482aa5cbdc93
anthonyg_sss <anthonyg_sss@b151f1c1-aa25-0410-a126-940d3bbd776e>
2007-01-05 17:17:08 +0000

    Changed Release config to allow cleaner, And logical release. Wrote class for DllLoader, Please try to follow this standard. Launcher now directory independent(doesn't need to be in Morrowind install location). Added new debug concept. Someone please compile and test this build. I'm not at my Morrowind Machine at the moment.

--- mwse 0.9.3/Loader/MWSEloader.cpp
+++ mwse 0.9.3/Loader/MWSEloader.cpp
@@ -1,82 +1,34 @@
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include "cDllLoader.h"
 
+/*
+AnthonyG 5-01-07
+Added silent and console modes
+To do silent define _SILENT
+To show a console window don't define anything
+Main MUST return an integer to exit properly, changed to int type
+*/
+
+//What functions do these macros cover? 
+//Look like debug macros, Use printf :). AnthonyG
+/*
 #define _ERROR
 #define _MESSAGE
+*/
 
-static void InjectDLL(DWORD pid) {
-	HANDLE	process = OpenProcess(
-		PROCESS_CREATE_THREAD|PROCESS_QUERY_INFORMATION|PROCESS_VM_OPERATION|PROCESS_VM_WRITE|PROCESS_VM_READ,false,pid);
-	if(process) {
-		DWORD	hookBase = (DWORD)VirtualAllocEx(process, NULL, 8192, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-		if(hookBase) {
-			DWORD	loadLibraryAAddr = (DWORD)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-
-			_MESSAGE("hookBase = %08X", hookBase);
-			_MESSAGE("loadLibraryAAddr = %08X", loadLibraryAAddr);
-
-			DWORD	bytesWritten;
-			WriteProcessMemory(process, (LPVOID)(hookBase + 5), "MWSE.dll" , 9, &bytesWritten);
-
-			BYTE	hookCode[5];
-
-			hookCode[0] = 0xE9;
-			*((DWORD*)&hookCode[1]) = loadLibraryAAddr - (hookBase + 5);
-
-			WriteProcessMemory(process, (LPVOID)(hookBase), hookCode, sizeof(hookCode), &bytesWritten);
-
-			HANDLE	thread = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)hookBase, (void *)(hookBase + 5), 0, NULL);
-			if(thread)
-			{
-					switch(WaitForSingleObject(thread, 5000))
-					{
-						case WAIT_OBJECT_0:
-							_MESSAGE("hook thread complete");
-							break;
-
-						case WAIT_ABANDONED:
-							_ERROR("Process::InstallHook: waiting for thread = WAIT_ABANDONED");
-							break;
-
-						case WAIT_TIMEOUT:
-							_ERROR("Process::InstallHook: waiting for thread = WAIT_TIMEOUT");
-							break;
-					}
-
-				CloseHandle(thread);
-			}
-			else
-				_ERROR("CreateRemoteThread failed (%d)", GetLastError());
-
-			VirtualFreeEx(process, (LPVOID)hookBase, 8192, MEM_RELEASE);
-		}
-		else
-			_ERROR("Process::InstallHook: couldn't allocate memory in target process");
-
-		CloseHandle(process);
-	}
-	else
-		_ERROR("Process::InstallHook: couldn't get process handle");
-}
-
-void main()
+#ifndef _SILENT
+int main()
+#else
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+#endif
 {
-	HWND mwwin = FindWindow("Morrowind", "Morrowind");
-	if(!mwwin) {
-		STARTUPINFO startup_info = { sizeof(STARTUPINFO), NULL, NULL, NULL, 
-						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 0 };
-		PROCESS_INFORMATION proc_info;
-		if(!CreateProcess("Morrowind Launcher.exe", "Morrowind Launcher.exe", 0, 0, false, NORMAL_PRIORITY_CLASS, 
-			0, 0, &startup_info, &proc_info)) _ERROR("Could not launch 'Morrowind Launcher.exe'");
-	}
-	while(!mwwin) {
-		Sleep(20);
-		mwwin = FindWindow("Morrowind", "Morrowind");
-	}
+	//Instantiate cDllLoader
+	cDllLoader iStartEmUp;
 
-	DWORD mwpid;
-	GetWindowThreadProcessId(mwwin, &mwpid);
+	iStartEmUp.mInitMorrowind();
 
-	InjectDLL(mwpid);
+	iStartEmUp.mInjectDll(iStartEmUp.vMorroID);
+
+	return 0;
 }
 
