009d956786da4358ba4f26807a83f84032614d42
anthonyg_sss <anthonyg_sss@b151f1c1-aa25-0410-a126-940d3bbd776e>
2007-01-06 19:24:37 +0000

    Updated proj file, Not sure for better or worse, Feel free to play with settings. main.cpp modified Cleaned up a bit, now uses nLogger::cHtml. nLogger added to source files. I'm frightened to play with the code anymore at the moment.

--- mwse 0.9.3/MWSE DLL/source/main.cpp
+++ mwse 0.9.3/MWSE DLL/source/main.cpp
@@ -1,14 +1,24 @@
-#include "LOG.h"
+#include "nLogger.h"
 #include "MWSEmain.h"
 
+/*
+ 1/6/07
+ AnthonyG:
+ I'm working on replacing LOG with nLogger.
+ Current class for logging is HTML, Feel free to change it.
+
+ Most of this needs to be cleaned up, Every line is defiled.
+ Timeslip: Please abide to the standard introduced in the Loader's cDllLoader .h/.cpp.
+*/
 //TODO: Check FUNCHASEQUIPEDPART2 works - uses dodgy breakpoint
 
-static const char WelcomeMessage[]= 
- "Morrowind Script Extender v0.9.3\n"
- "MWSE Copyright (C) 2003-2007 FreshFish copyleft GPL\n"
- "\nThis program is free software, licensed according to the GNU Public License,\n"
- "and comes with ABSOLUTELY NO WARRANTY.  See the file 'gpl.txt' for details.\n"
- "MWSE contributers: cdcooley, Fliggerty, Tp21, Timeslip and probably others\n";
+//AnthonyG: What do you know? Another useless static :)
+ char WelcomeMessage[]=
+	 "Morrowind Script Extender v0.9.3\n"
+     "MWSE Copyright (C) 2003-2007 FreshFish copyleft GPL\n"
+     "\nThis program is free software, licensed according to the GNU Public License,\n"
+     "and comes with ABSOLUTELY NO WARRANTY.  See the file 'gpl.txt' for details.\n"
+     "MWSE contributers: cdcooley, Fliggerty, Tp21, Timeslip and probably others\n";
 
 //Alternate injection method, for automatic loading without using MWSEloader.exe
 //use exports.def as the module definitions file, uncomment this function and rename the dll d3d8.dll
@@ -31,17 +41,22 @@ void* _stdcall FakeDirect3DCreate(UINT version) {
 
 BOOL _stdcall DllMain(HANDLE hModule, DWORD reason, void* unused)
 {
+	//1/6/07 AnthonyG: Instantiate HTML logger class
+	nLogger::cHtml MainFuncLog;
+
 	if(reason!=DLL_PROCESS_ATTACH) return true;
 
 	char* logname = "MWScriptExtender.log"; //logfile path, don't change because i can break some mods...
-	LOG::open(logname);
+	MainFuncLog.mOpen(logname);
 
-	LOG::log(WelcomeMessage);
-	LOG::log("\n\n\nMWSE dll attach start\n");
+	MainFuncLog.mAddCustom(WelcomeMessage,"#a9a9a9");
+	MainFuncLog.mAddCustom("\n\n\nMWSE dll attach start\n","#a9a9a9");
 
 	MWSEOnProcessStart();
 
-	LOG::log("MWSE dll attach end\n");
+	MainFuncLog.mAddCustom("MWSE dll attach end\n","#a9a9a9");
+
+	MainFuncLog.mClose();
 
 	return true;
 }
