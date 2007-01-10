5d196df8c55c7e62a01923e658237e68ecf1b3be
Darksecond <Darksecond@b151f1c1-aa25-0410-a126-940d3bbd776e>
2007-01-10 15:44:02 +0000

    added console version of Loader for myself.

---
+++ mwse 0.9.3/Loader Console/cDllLoader.h
@@ -0,0 +1,47 @@
+#ifndef _ERR_CDLLLOADER
+#define _ERR_CDLLLOADER
+#define _CRT_SECURE_NO_DEPRECATE
+/*********************************************\
+* AnthonyG:                                   *
+* Class that contains members to load the DLL *
+* Please add funcs to this class as needed    *
+\*********************************************/
+//Lean and mean specified in MWSEloader already
+#include<Windows.h>
+#include<StdIO.h>
+
+class cDllLoader
+{
+private:
+	//Later members may want to use these
+	HANDLE vProcess,vThread;
+	DWORD vBaseHook,vLoadLibraryAddr;
+	DWORD vBytesWritten;
+	BYTE vHookCode[5];
+	//Process items
+	HWND vMorroWin;
+	STARTUPINFO sSi; //See constructor definition
+	PROCESS_INFORMATION sPi;
+	//AnthonyG's MorroLocater vars
+	char vMorroLauncherLocation[1000]; //Will contain location of Morrowind Launcher
+	char vMorroLauncherFormattedLocation[1000]; //Use this, Has full location.
+	HKEY vKeyRes;
+	DWORD vSizeOfBuf;
+public:
+	DWORD vMorroID;
+
+	//Start up Morrowind. Guess what? Tis now directory dependent.(I.E. Place it in your desktop folder if you want!)
+	//Fills MorroID with Proc ID for use with InjectDll;
+	void mInitMorrowind();
+
+	//run program from commandline, and just sit until Morrowind starts
+	void mInitMorrowindCommandline(char* commandline);
+
+	//Inject DLL 
+	void mInjectDll(DWORD ProcID);
+
+	cDllLoader();
+	~cDllLoader();
+};
+
+#endif //_ERR_CDLLLOADER
---
+++ mwse 0.9.3/Loader Console/main.cpp
@@ -0,0 +1,75 @@
+#define WIN32_LEAN_AND_MEAN
+#include "cDllLoader.h"
+#include "cMailServer.h"
+
+/*
+AnthonyG 5-01-07
+Added silent and console modes
+To do silent define _SILENT
+To show a console window don't define anything
+Main MUST return an integer to exit properly, changed to int type
+*/
+
+//What functions do these macros cover? 
+//Look like debug macros, Use printf :). AnthonyG
+/*
+#define _ERROR
+#define _MESSAGE
+*/
+
+#ifndef _SILENT
+int main(int argc, char* argv[])
+#else
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+#endif
+{
+    #ifndef _SILENT
+	/*
+	Added by AnthonyG 05-4-07, 
+	Preprocessed to save that MicroSec or two of CPU time :).
+	Makes the console a bit more appealing to have a title
+	*/
+	SetConsoleTitle("Morrowind Script Extender");
+    #endif
+
+	//Message var, Don't change this
+	MSG vMsg;
+
+	//Instantiate cDllLoader
+	cDllLoader iStartEmUp;
+	//Instantiate server class
+	cMailServer iMail;
+
+	#ifndef _SILENT
+
+	//argc and argv don't exists when run in silent mode
+	if(argc > 1)
+	{
+		iStartEmUp.mInitMorrowindCommandline(argv[1]);
+	}
+	else
+	{
+		iStartEmUp.mInitMorrowind();
+	}
+	#else
+
+	iStartEmUp.mInitMorrowind();
+
+	#endif
+
+	iStartEmUp.mInjectDll(iStartEmUp.vMorroID);
+
+	//Create mail server(So we can get log msgs from the DLL)
+	iMail.mCreateServer();
+	while(GetMessage(&vMsg,0,0,0)>0)
+	{
+		iMail.mReadMail();
+		TranslateMessage(&vMsg);
+		DispatchMessage(&vMsg);
+	}
+	//Shutdown the server
+	iMail.mShutdownServer();
+
+	return 0;
+}
+
