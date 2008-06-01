0813aa4ab290176db30a8d7736fde9b351681e1f
darksecond <darksecond@b151f1c1-aa25-0410-a126-940d3bbd776e>
2008-06-01 20:23:52 +0000

    added referenced variables to VMExecuteInterface.h

--- MWSE/VMExecuteInterface.h
+++ MWSE/VMExecuteInterface.h
@@ -32,18 +32,24 @@ namespace mwse
 		//local variables, methods to access local variables
 		virtual mwLong_t getLongVariable(int index) = 0;
 		virtual mwLong_t getLongVariable(const char *id) = 0;
+		virtual mwLong_t getLongVariable(int index, REFRRecord_t &reference);
 		virtual void setLongVariable(int index, mwLong_t value) = 0;
 		virtual void setLongVariable(const char *id, mwLong_t value) = 0;
+		virtual void setLongVariable(int index, mwLong_t value , REFRRecord_t &reference);
 
 		virtual mwShort_t getShortVariable(int index) = 0;
 		virtual mwShort_t getShortVariable(const char *id) = 0;
+		virtual mwShort_t getShortVariable(int index, REFRRecord_t &reference);
 		virtual void setShortVariable(int index, mwShort_t value) = 0;
 		virtual void setShortVariable(const char *id, mwShort_t value) = 0;
+		virtual void setShortVariable(int index, mwShort_t value , REFRRecord_t &reference);
 
 		virtual mwFloat_t getFloatVariable(int index) = 0;
 		virtual mwFloat_t getFloatVariable(const char *id) = 0;
+		virtual mwFloat_t getFloatVariable(int index, REFRRecord_t &reference);
 		virtual void setFloatVariable(int index, mwFloat_t value) = 0;
 		virtual void setFloatVariable(const char *id, mwFloat_t value) = 0;
+		virtual void setFloatVariable(int index, mwFloat_t value , REFRRecord_t &reference);
 
 		//global variables, methods to access global variables
 		virtual mwLong_t getLongGlobal(const char *id) = 0;
@@ -56,8 +62,10 @@ namespace mwse
 		virtual void setFloatGlobal(const char *id, mwFloat_t value) = 0;
 
 		//objects and stuff
+		//look at GETREF function in old MWSE for examples on how to fetch references!
 		virtual void * getReference(const char *id) = 0; //can also be called getMorrowindObject i guess, this fetches the correct reference for the given object (player, npc, book, light, etc)
 		virtual void * getReference() = 0; //gets the current reference, maybe it's better to put this in another function. it'll do for now
+		
 		virtual void setReference(void * reference) = 0; //this sets the reference, to be used for the next instruction (also works on other MWSE instructions in the same run), but for all other things, this needs to be called before the function! (as it needs a run in runScript)
 		
 		//getParameters
