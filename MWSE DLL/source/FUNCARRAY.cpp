ca8ed2eba126280bdaf47cc9f1562f0c72589b54
Merzasphor <merzasphor@openmailbox.org>
2015-06-15 20:29:43 -0400

    Simplified array impmentation. Arrays will now grow as needed.

--- MWSE DLL/source/FUNCARRAY.cpp
+++ MWSE DLL/source/FUNCARRAY.cpp
@@ -1,59 +1,34 @@
 #include "FUNCARRAY.h"
 #include "cLog.h"
 
-#include <map>
+#include <vector>
 
 long const MAXARRAYID = 16777215; // max 24 bit int - avoid exceding MW global precision
 
-struct MWSEArray
-{
-	enum ArrayTypes
-	{
-		Float,
-		Long
-	};
-	ArrayTypes type;
-	long size;
-	void * data;
-};
-
-static std::map<long, MWSEArray> arrays;
+static std::vector< std::vector< long > > arrays;
 
 bool FUNCCREATEARRAY::execute(void)
 {
-	long size;
-	long type;
 	long id = 0;
+	long newId;
 
-	if (machine.pop(type) && type >= MWSEArray::Float && type <= MWSEArray::Long &&
-		machine.pop(size) && size > 0)
+	if (arrays.empty())
+	{ 
+		newId = 1;
+	}
+	else
 	{
-		long newId;
-		if (arrays.empty())
-		{ 
-			newId = 1;
-		}
-		else
-		{
-			newId = 1 + arrays.rbegin()->first;
-		}
+		newId = 1 + arrays.size();
+	}
 
-		if (newId <= MAXARRAYID)
-		{
-			id = newId;
-			MWSEArray newArray;
-			newArray.type = static_cast<MWSEArray::ArrayTypes>(type);
-			newArray.size = size;
-			if (newArray.type == MWSEArray::Float)
-			{
-				newArray.data = reinterpret_cast<void*>(new float[newArray.size]);
-			}
-			else if (newArray.type == MWSEArray::Long)
-			{
-				newArray.data = reinterpret_cast<void*>(new long[newArray.size]);
-			}
-			arrays[id] = newArray;
-		}
+	if (newId <= MAXARRAYID)
+	{
+		id = newId;
+		arrays.push_back(std::vector<long>());
+	}
+	else
+	{
+		cLog::mLogMessage("xCreateArray: Unable to create array. id: %d", id);
 	}
 
 #ifdef DEBUGGING
@@ -67,71 +42,48 @@ bool FUNCGETARRAYVALUE::execute(void)
 {
 	long id;
 	long index;
+	long value = 0;
 
-	if (machine.pop(id) && id > 0 && id <= MAXARRAYID
-		&& machine.pop(index) && index >= 0)
+	if (machine.pop(id) && machine.pop(index))
 	{
-		MWSEArray const & a = arrays[id];
-		if (index <= a.size - 1)
+		if ((id > 0 && id <= arrays.size()))
 		{
-			if (a.type == MWSEArray::Float)
-			{
-				float * data = reinterpret_cast<float*>(a.data);
-				return(machine.push(data[index]));
+			std::vector<long> const & a = arrays[id - 1];
+			if (index >= 0 && index < a.size())
+			{	
+				if (index <= a.size() - 1)
+				{
+					value = a[index];
+				}
+				else
+				{
+					cLog::mLogMessage("xGetArrayValue: Array index out of bounds. id: %d index: %d", id, index);
+				}
 			}
-			else if (a.type == MWSEArray::Long)
-			{
-				long * data = reinterpret_cast<long*>(a.data);
-				return(machine.push(data[index]));
-			}
-		}
-		else
-		{
-			cLog::mLogMessage("xGetArrayValue: Array index out of bounds. id: %d index: %d", id, index);
 		}
 	}
 
-	return (machine.push(static_cast<VMREGTYPE>(0)));
+	return (machine.push(value));
 }
 
 bool FUNCSETARRAYVALUE::execute(void)
 {
 	long id;
 	long index;
+	long value;
 	bool result = false;
 
 	if (machine.pop(id) && id > 0 && id <= MAXARRAYID
-		&& machine.pop(index) && index >= 0)
+		&& machine.pop(index) && index >= 0
+		&& machine.pop(value))
 	{
-		MWSEArray const & a = arrays[id];
-		if (index <= a.size - 1)
+		std::vector<long> & a = arrays[id - 1];
+		if (index + 1 > a.size())
 		{
-			if (a.type == MWSEArray::Float)
-			{
-				float value;
-				if (machine.pop(value))
-				{
-					float * data = reinterpret_cast<float*>(a.data);
-					data[index] = value;
-					result = true;
-				}
-				
-			}
-			else if (a.type == MWSEArray::Long)
-			{
-				long value;
-				if (machine.pop(value))
-				{
-					long * data = reinterpret_cast<long*>(a.data);
-					data[index] = value;
-					result = true;
-				}
-			}
+			a.resize(index + 1);
 		}
-		else
-		{
-			cLog::mLogMessage("xSetArrayValue: Array index out of bounds. id: %d index: %d", id, index);
-		}
+		a[index] = value;
+		result = true;
 	}
 
 	return (machine.push(static_cast<VMREGTYPE>(result)));
