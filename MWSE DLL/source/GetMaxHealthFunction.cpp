57b8d6851bfa98e962f93e85ed15896b07942aa4
Merzasphor <merzasphor@openmailbox.org>
2017-10-14 18:56:18 -0400

    xGetMaxHealth now works with creatures.

--- MWSE DLL/source/GetMaxHealthFunction.cpp
+++ MWSE DLL/source/GetMaxHealthFunction.cpp
@@ -6,15 +6,15 @@
 
 bool GetMaxHealthFunction::execute(void)
 {
-	float value = -1.0;
-	unsigned long type;
-	VPVOID refr, temp;
-	if (GetTargetData(machine, &refr, &temp, &type) && type == NPC) {
+	float health = -1.0;
+	unsigned long record_type;
+	VPVOID reference, temp;
+	if (GetTargetData(machine, &reference, &temp, &record_type) &&
+		(record_type == NPC || record_type == CREATURE)) {
 		MACPRecord* macp = 
-			reinterpret_cast<MACPRecord*>(GetAttachPointer(machine, refr, MACHNODE));
-		if (macp) {
-			value = macp->health.base;
-		}
+			reinterpret_cast<MACPRecord*>(GetAttachPointer(
+			machine, reference, MACHNODE));
+		if (macp != NULL) health = macp->health.base;
 	}
-	return (machine.push(value));
+	return (machine.push(health));
 }
