23f86f78265c92360355b673e7bd491686e5a779
Merzasphor <merzasphor@openmailbox.org>
2017-10-14 20:18:15 -0400

    Code cleanup.

--- MWSE DLL/source/ModAttributeFunction.cpp
+++ MWSE DLL/source/ModAttributeFunction.cpp
@@ -8,19 +8,20 @@ bool ModAttributeFunction::execute(void)
 {
 	long attribute_index;
 	long result = 0;
-	float value;
-	unsigned long type;
-	VPVOID refr, temp;
-	if (GetTargetData(machine, &refr, &temp, &type) && (type == NPC || type == CREATURE)) {
-		MACPRecord* macp =
-			reinterpret_cast<MACPRecord*>(GetAttachPointer(machine, refr, MACHNODE));
-		if (macp && machine.pop(attribute_index) && machine.pop(value)
+	float mod_value;
+	unsigned long record_type;
+	VPVOID reference, temp;
+	if (GetTargetData(machine, &reference, &temp, &record_type) &&
+		(record_type == NPC || record_type == CREATURE)) {
+		MACPRecord* const macp =
+			reinterpret_cast<MACPRecord*>(GetAttachPointer(machine, reference, MACHNODE));
+		if (macp != NULL && machine.pop(attribute_index) && machine.pop(mod_value)
 			&& attribute_index >= kFirstAttribute 
 			&& attribute_index <= kLastAttribute) {
-			float new_current = macp->attributes[attribute_index].current + value;
+			float new_current = macp->attributes[attribute_index].current + mod_value;
 			if (new_current < 0) new_current = 0;
 			macp->attributes[attribute_index].current = new_current;
-			float new_base = macp->attributes[attribute_index].base + value;
+			float new_base = macp->attributes[attribute_index].base + mod_value;
 			if (new_base < 0) new_base = 0;
 			macp->attributes[attribute_index].base = new_base;
 			result = 1;
