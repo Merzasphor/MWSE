8fabb1bbcf31ef6f5cf1e90f7dc38e05a9808d0e
NullCascade <nullcascade@gmail.com>
2017-11-20 21:49:00 -0800

    xAnd/xOr: Fix logic error that would result in not all variables being popped.

--- MWSE/xAnd.cpp
+++ MWSE/xAnd.cpp
@@ -45,10 +45,16 @@ namespace mwse
 	float xAnd::execute(mwse::VMExecuteInterface &virtualMachine)
 	{
 		if (mwse::Stack::getInstance().size() != 2) {
+#if _DEBUG
+			mwse::log::getLog() << "Warning: xAnd - Called with " << mwse::Stack::getInstance().size() << " parameters." << std::endl;
+#endif
 			return 0;
 		}
 
-		mwse::Stack::getInstance().pushLong(mwse::Stack::getInstance().popLong() && mwse::Stack::getInstance().popLong());
+		mwLong_t param1 = mwse::Stack::getInstance().popLong();
+		mwLong_t param2 = mwse::Stack::getInstance().popLong();
+
+		mwse::Stack::getInstance().pushLong(param1 && param2);
 
 		return 0.0;
 	}
