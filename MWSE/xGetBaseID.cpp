79b7db514994604f944c63a147378dd6220af80c
NullCascade <nullcascade@gmail.com>
2017-11-26 15:33:50 -0800

    Add xGetBaseID.

---
+++ MWSE/xGetBaseID.cpp
@@ -0,0 +1,71 @@
+/************************************************************************
+
+	xGetBaseID.cpp - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+
+using namespace mwse;
+
+namespace mwse
+{
+	class xGetBaseID : mwse::InstructionInterface_t
+	{
+	public:
+		xGetBaseID();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	};
+
+	static const mwse::mwOpcode_t xGetBaseIDOpcode = 0x3F6F;
+	static xGetBaseID xGetBaseIDInstance;
+
+	xGetBaseID::xGetBaseID() : mwse::InstructionInterface_t(xGetBaseIDOpcode) {}
+
+	void xGetBaseID::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	float xGetBaseID::execute(mwse::VMExecuteInterface &virtualMachine)
+	{
+		// Get reference.
+		REFRRecord_t* reference = virtualMachine.getReference();
+		char* objectId = NULL;
+
+		// Try to get the data base record objectId first, and fall back to the copy object.
+		TES3DefaultTemplate_t* data = reinterpret_cast<TES3DefaultTemplate_t*>(reference->recordPointer);
+		if (data && reinterpret_cast<NPCCopyRecord_t*>(data)->baseNPC) {
+			objectId = reinterpret_cast<NPCCopyRecord_t*>(data)->baseNPC->objectId;
+		}
+		else if (data && data->objectId) {
+			objectId = data->objectId;
+		}
+
+		// Push the base value of that skill.
+		if (objectId) {
+			mwse::Stack::getInstance().pushString(objectId);
+		}
+		else {
+			mwse::Stack::getInstance().pushString("unknown");
+		}
+
+		return 0.0f;
+	}
+}
