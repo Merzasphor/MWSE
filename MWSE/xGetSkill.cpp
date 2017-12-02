ce516b0f4b86070c75a59fb03390613038dbdef6
NullCascade <nullcascade@gmail.com>
2017-12-01 16:48:36 -0800

    Add xGetSkill.

---
+++ MWSE/xGetSkill.cpp
@@ -0,0 +1,79 @@
+/************************************************************************
+
+xGetSkill.cpp - Copyright (c) 2008 The MWSE Project
+http://www.sourceforge.net/projects/mwse
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+
+using namespace mwse;
+
+namespace mwse
+{
+	class xGetSkill : mwse::InstructionInterface_t
+	{
+	public:
+		xGetSkill();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	private:
+		const mwFloat_t INVALID_VALUE = -1.0f;
+	};
+
+	static const mwse::mwOpcode_t xGetSkillOpcode = 0x3950;
+	static xGetSkill xGetSkillInstance;
+
+	xGetSkill::xGetSkill() : mwse::InstructionInterface_t(xGetSkillOpcode) {}
+
+	void xGetSkill::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	float xGetSkill::execute(mwse::VMExecuteInterface &virtualMachine)
+	{
+		// Get skill id argument.
+		mwLong_t skillId = mwse::Stack::getInstance().popLong();
+		if (skillId < FirstSkill || skillId > LastSkill) {
+			mwse::log::getLog() << "xGetSkill: Invalid skill id: " << skillId << std::endl;
+			mwse::Stack::getInstance().pushFloat(INVALID_VALUE);
+			return 0.0f;
+		}
+		
+		// Get reference.
+		mwse::REFRRecord_t* reference = virtualMachine.getReference();
+		if (reference->recordPointer->recordType != RecordTypes::NPC && reference->recordPointer->recordType != RecordTypes::CREATURE) {
+			mwse::log::getLog() << "xGetSkill: Reference is not a creature or NPC." << std::endl;
+			mwse::Stack::getInstance().pushFloat(INVALID_VALUE);
+			return 0.0f;
+		}
+
+		// Get the associated MACP record.
+		MACPRecord_t* macp = tes3::getAttachedMACPRecord(reference);
+		if (macp == NULL) {
+			mwse::log::getLog() << "xGetSkill: Could not find MACP record for reference." << std::endl;
+			mwse::Stack::getInstance().pushFloat(INVALID_VALUE);
+			return 0.0f;
+		}
+
+		// Push the current value of that skill.
+		mwse::Stack::getInstance().pushFloat(macp->skills[skillId].current);
+
+		return 0.0f;
+	}
+}
