7f3b04d4aa8ebba3acbd058ab12d9296074352f6
NullCascade <nullcascade@gmail.com>
2017-12-02 19:44:48 -0800

    Add xSetCharge.

---
+++ MWSE/xSetCharge.cpp
@@ -0,0 +1,80 @@
+/************************************************************************
+
+xSetCharge.cpp - Copyright (c) 2008 The MWSE Project
+http://www.sourceforge.net/projects/mwse
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+
+using namespace mwse;
+
+namespace mwse {
+	class xSetCharge : mwse::InstructionInterface_t {
+	public:
+		xSetCharge();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	};
+
+	static const mwse::mwOpcode_t xSetChargeOpcode = 0x3E66;
+	static xSetCharge xSetChargeInstance;
+
+	xSetCharge::xSetCharge() : mwse::InstructionInterface_t(xSetChargeOpcode) {}
+
+	void xSetCharge::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	float xSetCharge::execute(mwse::VMExecuteInterface &virtualMachine) {
+		// Get charge from parameter.
+		mwFloat_t charge = Stack::getInstance().popFloat();
+
+		// Get reference.
+		REFRRecord_t* reference = virtualMachine.getReference();
+		if (reference == NULL) {
+			mwse::log::getLog() << "xSetCharge: No reference provided." << std::endl;
+			mwse::Stack::getInstance().pushShort(0);
+			return 0.0f;
+		}
+
+		// Get the base record.
+		TES3DefaultTemplate_t* record = reinterpret_cast<TES3DefaultTemplate_t*>(reference->recordPointer);
+		if (record == NULL) {
+			mwse::log::getLog() << "xSetCharge: No record found for reference." << std::endl;
+			mwse::Stack::getInstance().pushShort(0);
+			return 0.0f;
+		}
+
+		// Get the charge based on the record type. If the item doesn't have a varnode,
+		// return the maximum charge from the enchantment record.
+		mwVarHolderNode_t* varNode = mwse::tes3::getAttachedVarHolderNode(reference);
+		if (varNode) {
+			*reinterpret_cast<mwFloat_t*>(&varNode->unknown_0x10) = charge;
+		}
+		else {
+			mwse::log::getLog() << "xSetCharge: Could not get attached VARNODE." << std::endl;
+			mwse::Stack::getInstance().pushShort(0);
+			return 0.0f;
+		}
+
+		mwse::Stack::getInstance().pushShort(1);
+
+		return 0.0f;
+	}
+}
