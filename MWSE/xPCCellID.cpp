d8e24ebd68fd45cf424e90dd1f66f9ef9a438df6
NullCascade <nullcascade@gmail.com>
2017-12-02 17:22:29 -0800

    Add xPCCellID.

---
+++ MWSE/xPCCellID.cpp
@@ -0,0 +1,68 @@
+/************************************************************************
+	
+	xPCCellID.cpp - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+
+using namespace mwse;
+
+namespace mwse
+{
+	class xPCCellID : mwse::InstructionInterface_t
+	{
+	public:
+		xPCCellID();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	};
+
+	static const mwse::mwOpcode_t xPCCellIDOpcode = 0x3C2D;
+	static xPCCellID xPCCellIDInstance;
+
+	xPCCellID::xPCCellID() : mwse::InstructionInterface_t(xPCCellIDOpcode) {}
+
+	void xPCCellID::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	float xPCCellID::execute(mwse::VMExecuteInterface &virtualMachine)
+	{
+		REFRRecord_t* reference = virtualMachine.getReference();
+
+		TES3CellMaster_t* masterCell = tes3::getCellMaster();
+		if (masterCell == NULL) {
+			mwse::log::getLog() << "xPCCellID: Cell master could not be found." << std::endl;
+			Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+
+		TES3Cell_t* cell = masterCell->interiorCell;
+		if (!cell) {
+			cell = masterCell->exteriorCells[CENTER]->first;
+		}
+
+		mwseString_t cellId = cell->cellname;
+
+		Stack::getInstance().pushLong(cellId);
+
+		return 0.0f;
+	}
+}
