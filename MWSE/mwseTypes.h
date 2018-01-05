590d596aa0e24dbc88544a0a92aa1e61fa73011c
NullCascade <nullcascade@gmail.com>
2018-01-05 14:15:04 -0800

    mwseTypes: Add SPLLRecord_t and SPLLNode_t.

--- MWSE/mwseTypes.h
+++ MWSE/mwseTypes.h
@@ -62,6 +62,7 @@ namespace mwse
 			SKIL = 'LIKS', SKILL = SKIL,
 			SNDG = 'GDNS', SOUNDGENERATOR = SNDG,
 			SPEL = 'LEPS', SPELL = SPEL,
+			SPLL = 'LLPS',
 			STAT = 'TATS', STATIC = STAT,
 			WEAP = 'PAEW', WEAPON = WEAP
 		};
@@ -630,6 +631,69 @@ namespace mwse
 		long flags;	// 0xF4 // 1=AUTOCALC, 2=PCSTART, 4=ALWAYSSUCCEEDS
 	};
 	static_assert(sizeof(SPELRecord_t) == 0xF8, "SPELRecord_t failed size validation");
+
+	struct SPLLRecord_t
+	{
+		struct ActiveSpell_t
+		{
+			char * id; // 0x00 // "PlayerSaveGame" if player.
+			REFRRecord_t * reference; // 0x04
+			mwFloat_t statistic; // 0x08 // Magicka Resistance if applicable.
+			unsigned long magnitude; // 0x0C
+			mwFloat_t time; // 0x10 // Elapsed? Start?
+		};
+		struct Effect_t
+		{
+			void * vTable; // 0x00
+			unsigned long inUse; // 0x04 // Number of null-pointers in array?
+			unsigned long size; // 0x08
+			ActiveSpell_t ** activeSpells; // 0x0C
+		};
+		void * vTable; // 0x00
+		RecordTypes::recordType_t recordType; // 0x04
+		long unknown_0x08;
+		long unknown_0x0C;
+		long unknown_0x10;
+		long unknown_0x14;
+		long unknown_0x18;
+		Effect_t effects[8]; // 0x1C
+		long unknown_0x9C;
+		SPELRecord_t * spell; // 0xA0
+		long unknown_0xA4;
+		long unknown_0xA8; // nth active effect? In cell?
+		long unknown_0xAC;
+		long unknown_0xB0;
+		long unknown_0xB4;
+		REFRRecord_t * reference; // 0xB8
+		unsigned long flags; // 0xBC
+		long unknown_0xC0;
+		long unknown_0xC4;
+		long unknown_0xC8;
+		long unknown_0xCC;
+		long unknown_0xD0;
+		long unknown_0xD4;
+		long unknown_0xD8;
+		long unknown_0xDC;
+		long unknown_0xE0;
+		char id[32]; // 0xE4 // Matches spell.id?
+	};
+	static_assert(sizeof(SPLLRecord_t::ActiveSpell_t) == 0x14, "SPLLRecord_t::ActiveSpell_t failed size validation");
+	static_assert(sizeof(SPLLRecord_t::Effect_t) == 0x10, "SPLLRecord_t::Effect_t failed size validation");
+	static_assert(sizeof(SPLLRecord_t) == 0x0104, "SPLLRecord_t failed size validation");
+
+	// Some kind of tree?
+	struct SPLLNode_t
+	{
+		SPLLNode_t * first; // 0x00
+		SPLLNode_t * second; // 0x04
+		SPLLNode_t * third; // 0x08
+		unsigned long id; // 0x0C
+		SPLLRecord_t * record; // 0x10
+		unsigned long flag; // 0x14
+		long unknown_0x18;
+		long unknown_0x1C;
+	};
+	static_assert(sizeof(SPLLNode_t) == 0x20, "SPLLNode_t failed size validation");
 
 	struct ALCHRecord_t
 	{
