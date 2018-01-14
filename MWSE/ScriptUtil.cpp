81aefcd622ac0c2d3663206b27ed5501eeeca5b6
NullCascade <nullcascade@gmail.com>
2018-01-13 16:03:16 -0800

    Strip useless documentation from ScriptUtil. Add header.

--- MWSE/ScriptUtil.cpp
+++ MWSE/ScriptUtil.cpp
@@ -1,3 +1,23 @@
+/************************************************************************
+	
+	ScriptUtil.cpp - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
 
 #include "ScriptUtil.h"
 
@@ -5,37 +25,30 @@ namespace mwse
 {
 	namespace mwscript
 	{
-		// 
 		int getInstructionPointer() {
 			return *reinterpret_cast<int*>(TES3_IP_IMAGE);
 		}
 
-		// 
 		void setInstructionPointer(int IP) {
 			*reinterpret_cast<int*>(TES3_IP_IMAGE) = IP;
 		}
 
-		// 
 		TES3DefaultTemplate_t* getScriptSecondObject() {
 			return *reinterpret_cast<TES3DefaultTemplate_t**>(TES3_SECONDOBJECT_IMAGE);
 		}
 
-		// 
 		void setScriptSecondObject(TES3DefaultTemplate_t* record) {
 			*reinterpret_cast<TES3DefaultTemplate_t**>(TES3_SECONDOBJECT_IMAGE) = record;
 		}
 
-		// 
 		mwLong_t getScriptVariableIndex() {
 			return *reinterpret_cast<mwLong_t*>(TES3_VARINDEX_IMAGE);
 		}
 
-		// 
 		void setScriptVariableIndex(mwLong_t index) {
 			*reinterpret_cast<mwLong_t*>(TES3_VARINDEX_IMAGE) = index;
 		}
 
-		// 
 		float RunOriginalOpCode(SCPTRecord_t* script, float unk1, float unk2, int opCode, char charParam, REFRRecord_t* reference) {
 			float result = 0.0;
 
@@ -58,7 +71,6 @@ namespace mwse
 			return result;
 		}
 
-		// 
 		void AddItem(SCPTRecord_t* script, REFRRecord_t* reference, TES3DefaultTemplate_t* itemTemplate, mwLong_t count) {
 			// Cache previous script variables.
 			mwLong_t cachedVarIndex = getScriptVariableIndex();
--- MWSE/ScriptUtil.h
+++ MWSE/ScriptUtil.h
@@ -1,3 +1,23 @@
+/************************************************************************
+	
+	ScriptUtil.h - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
 
 #pragma once
 
@@ -9,28 +29,17 @@ namespace mwse
 {
 	namespace mwscript
 	{
-		//
 		int getInstructionPointer();
-
-		//
 		void setInstructionPointer(int IP);
 
-		//
 		TES3DefaultTemplate_t* getScriptSecondObject();
-
-		//
 		void setScriptSecondObject(TES3DefaultTemplate_t* record);
 
-		// 
 		mwLong_t getScriptVariableIndex();
-
-		// 
 		void setScriptVariableIndex(mwLong_t index);
 
-		//
 		float RunOriginalOpCode(SCPTRecord_t* script, float unk1, float unk2, int opCode, char charParam, REFRRecord_t* reference);
 
-		//
 		void AddItem(SCPTRecord_t* script, REFRRecord_t* reference, TES3DefaultTemplate_t* itemTemplate, mwLong_t count);
 	}
 }
