38e7baaa7242c9ea9bc71f6697fa067d1a0fb11d
NullCascade <nullcascade@gmail.com>
2018-01-19 22:06:58 -0800

    Add xTextInputAlt.

---
+++ MWSE/xTextInputAlt.cpp
@@ -0,0 +1,120 @@
+/************************************************************************
+	
+	xTextInputAlt.cpp - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+#include "StringUtil.h"
+
+#include <map>
+#include <Windows.h>
+
+using namespace mwse;
+
+namespace mwse {
+	class xTextInputAlt : mwse::InstructionInterface_t {
+	public:
+		xTextInputAlt();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	};
+
+	static const mwse::mwOpcode_t xTextInputAltOpcode = 0x3F02;
+	static xTextInputAlt xTextInputAltInstance;
+
+	xTextInputAlt::xTextInputAlt() : mwse::InstructionInterface_t(xTextInputAltOpcode) {}
+
+	void xTextInputAlt::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	static std::map<int, char> keyCharMap;
+
+	bool GetKeyIsPressed(int VK_key) {
+		return (GetAsyncKeyState(VK_key) & 0x8001) == 0x8001;
+	}
+
+	float xTextInputAlt::execute(mwse::VMExecuteInterface &virtualMachine) {
+		// 1st parameter: Message Id.
+		mwseString_t& message = virtualMachine.getString(mwse::Stack::getInstance().popLong());
+
+		// 2nd parameter: Key to use to end the input stream. 
+		mwLong_t endCode = mwse::Stack::getInstance().popLong();
+
+		// The default endcode is return.
+		if (endCode == 0) {
+			endCode = VK_RETURN;
+		}
+
+		// Determine if the end keycode is down, and break out if so.
+		if (GetKeyIsPressed(endCode)) {
+			mwse::Stack::getInstance().pushString(message);
+			mwse::Stack::getInstance().pushLong(message.length());
+			return 0.0f;
+		}
+
+		// If the back key is pressed, remove a character.
+		if (GetKeyIsPressed(VK_BACK)) {
+			if (message.length() > 0) {
+				message.erase(message.end() - 1);
+			}
+			mwse::Stack::getInstance().pushString(message);
+			mwse::Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+
+		// If the enter key is pressed, insert a newline.
+		if (GetKeyIsPressed(VK_RETURN)) {
+			message.append("\r\n");
+			mwse::Stack::getInstance().pushString(message);
+			mwse::Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+
+		// Handle spaces.
+		if (GetKeyIsPressed(VK_SPACE)) {
+			message.append(" ");
+			mwse::Stack::getInstance().pushString(message);
+			mwse::Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+
+		// Handle all other keys.
+		for (int i = 0x30; i <= 0x6F; i++) {
+			if (GetKeyIsPressed(i)) {
+				char character = MapVirtualKey(i, MAPVK_VK_TO_CHAR);
+
+				// Lowercase character if shift isn't pressed.
+				if (!GetAsyncKeyState(VK_SHIFT)) {
+					character = tolower(character);
+				}
+
+				if (character > 0) {
+					message.push_back(character);
+					mwse::Stack::getInstance().pushString(message);
+					mwse::Stack::getInstance().pushLong(0);
+					return 0.0f;
+				}
+			}
+		}
+
+		return 0.0f;
+	}
+}
