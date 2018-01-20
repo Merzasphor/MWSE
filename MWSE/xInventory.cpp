818fa7e39f239b07b2b3433611a2f08a65c0b92f
NullCascade <nullcascade@gmail.com>
2018-01-19 23:37:04 -0800

    Add xInventory and xNextStack.

---
+++ MWSE/xInventory.cpp
@@ -0,0 +1,73 @@
+/************************************************************************
+	
+	xInventory.cpp - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+
+using namespace mwse;
+
+namespace mwse
+{
+	class xInventory : mwse::InstructionInterface_t
+	{
+	public:
+		xInventory();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	};
+
+	static const mwse::mwOpcode_t xInventoryOpcode = 0x3C2A;
+	static xInventory xInventoryInstance;
+
+	xInventory::xInventory() : mwse::InstructionInterface_t(xInventoryOpcode) {}
+
+	void xInventory::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	float xInventory::execute(mwse::VMExecuteInterface &virtualMachine)
+	{
+		// Get reference.
+		REFRRecord_t* reference = virtualMachine.getReference();
+		if (reference == NULL) {
+			mwse::log::getLog() << "xInventory: Invalid reference attachment." << std::endl;
+			mwse::Stack::getInstance().pushLong(0);
+			mwse::Stack::getInstance().pushLong(0);
+			mwse::Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+		
+		ListNode_t<InventoryNode_t>* firstItem = tes3::getFirstInventoryNode(reference);
+		if (firstItem == NULL) {
+			mwse::log::getLog() << "xInventory: First item could not be determined." << std::endl;
+			mwse::Stack::getInstance().pushLong(0);
+			mwse::Stack::getInstance().pushLong(0);
+			mwse::Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+
+		mwse::Stack::getInstance().pushLong((mwLong_t)firstItem->nextNode);
+		mwse::Stack::getInstance().pushLong(firstItem->dataPtr->itemCount);
+		mwse::Stack::getInstance().pushString(reinterpret_cast<TES3DefaultTemplate_t*>(firstItem->dataPtr->recordAddress)->objectId);
+
+		return 0.0f;
+	}
+}
---
+++ MWSE/xNextStack.cpp
@@ -0,0 +1,64 @@
+/************************************************************************
+	
+	xNextStack.cpp - Copyright (c) 2008 The MWSE Project
+	http://www.sourceforge.net/projects/mwse
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+#include "VMExecuteInterface.h"
+#include "Stack.h"
+#include "InstructionInterface.h"
+#include "TES3Util.h"
+
+using namespace mwse;
+
+namespace mwse
+{
+	class xNextStack : mwse::InstructionInterface_t
+	{
+	public:
+		xNextStack();
+		virtual float execute(VMExecuteInterface &virtualMachine);
+		virtual void loadParameters(VMExecuteInterface &virtualMachine);
+	};
+
+	static const mwse::mwOpcode_t xNextStackOpcode = 0x3C2B;
+	static xNextStack xNextStackInstance;
+
+	xNextStack::xNextStack() : mwse::InstructionInterface_t(xNextStackOpcode) {}
+
+	void xNextStack::loadParameters(mwse::VMExecuteInterface &virtualMachine) {}
+
+	float xNextStack::execute(mwse::VMExecuteInterface &virtualMachine)
+	{
+		// Get the passed node.
+		ListNode_t<InventoryNode_t>* node = reinterpret_cast<ListNode_t<InventoryNode_t>*>(mwse::Stack::getInstance().popLong());
+		if (node == NULL) {
+			mwse::log::getLog() << "xNextStack: Node isn't valid." << std::endl;
+			mwse::Stack::getInstance().pushLong(0);
+			mwse::Stack::getInstance().pushLong(0);
+			mwse::Stack::getInstance().pushLong(0);
+			return 0.0f;
+		}
+
+		mwse::Stack::getInstance().pushLong((mwLong_t)node->nextNode);
+		mwse::Stack::getInstance().pushLong(node->dataPtr->itemCount);
+		mwse::Stack::getInstance().pushString(reinterpret_cast<TES3DefaultTemplate_t*>(node->dataPtr->recordAddress)->objectId);
+
+		return 0.0f;
+	}
+}
