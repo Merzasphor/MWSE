7076d10f40aa9f15810abddb2a59d72fadb80f57
NullCascade <nullcascade@gmail.com>
2018-03-18 16:40:08 -0700

    Adjust data structures for Light objects. Add functions for setting light flags.

--- MWSE/TES3Light.h
+++ MWSE/TES3Light.h
@@ -1,26 +1,69 @@
 #pragma once
 
 #include "TES3Object.h"
+#include "TES3Collections.h"
 
 namespace TES3 {
+	namespace LightFlags {
+		enum LightFlags {
+			Dynamic = 0x1,
+			CanCarry = 0x2,
+			Negative = 0x4,
+			Flicker = 0x8,
+			Fire = 0x10,
+			OffByDefault = 0x20,
+			FlickerSlow = 0x40,
+			Pulse = 0x80,
+			PulseSlow = 0x100
+		};
+	}
+
 	struct Light : PhysicalObject {
-		int unknown_0x30;
-		int unknown_0x34;
-		int unknown_0x38;
-		int unknown_0x3C;
-		int unknown_0x40;
-		int unknown_0x44;
+		void * animationData;
+		Iterator<void> stolenList; // 0x30
 		char* name; // 0x48
-		int unknown_0x4C;
-		char* model; // 0x50
+		char* model; // 0x4C
+		Script * script;
 		char* icon; // 0x54
 		float weight; // 0x58
 		long value; // 0x5C
-		int unknown_0x60;
-		int unknown_0x64;
-		int unknown_0x68;
-		int unknown_0x6C;
-		int unknown_0x70;
+		int time; // 0x60
+		int radius; // 0x64
+		signed char color[4]; // 0x68
+		unsigned int flags; // 0x6C
+		void * sound; // 0x70
+
+		//
+		// Functions to get/set flags.
+		//
+
+		bool getIsDynamic();
+		void setIsDynamic(bool);
+
+		bool getCanCarry();
+		void setCanCarry(bool);
+
+		bool getIsNegative();
+		void setIsNegative(bool);
+
+		bool getFlickers();
+		void setFlickers(bool);
+
+		bool getIsFire();
+		void setIsFire(bool);
+
+		bool getIsOffByDefault();
+		void setIsOffByDefault(bool);
+
+		bool getFlickersSlowly();
+		void setFlickersSlowly(bool);
+
+		bool getPulses();
+		void setPulses(bool);
+
+		bool getPulsesSlowly();
+		void setPulsesSlowly(bool);
+
 	};
 	static_assert(sizeof(Light) == 0x74, "TES3::Light failed size validation");
 }
