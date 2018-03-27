0ddfc234d61ded8be5ca9bb5064a1670e90807a5
NullCascade <nullcascade@gmail.com>
2018-03-27 00:27:21 -0700

    Add data for TES3::Regions.

---
+++ MWSE/TES3Region.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "TES3Object.h"
+
+namespace TES3 {
+	struct Region : BaseObject {
+		char id[32]; // 0x10
+		char name[32]; // 0x30
+		unsigned char weatherChanceClear; // 0x50
+		unsigned char weatherChanceCloudy; // 0x51
+		unsigned char weatherChanceFoggy; // 0x52
+		unsigned char weatherChanceOvercast; // 0x53
+		unsigned char weatherChanceRain; // 0x54
+		unsigned char weatherChanceThunder; // 0x55
+		unsigned char weatherChanceAsh; // 0x56
+		unsigned char weatherChanceBlight; // 0x57
+		unsigned char weatherChanceSnow; // 0x58
+		unsigned char weatherChanceBlizzard; // 0x59
+		unsigned char unknown_0x5A;
+		unsigned char unknown_0x5B;
+		Actor * sleepCreature; // 0x5C
+		int unknown_0x60[5]; // Unknown2 in Master?
+		int unknown_0x74;
+	};
+	static_assert(sizeof(Region) == 0x78, "TES3::Region failed size validation");
+}
