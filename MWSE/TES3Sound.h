bedd78d2656486ec07dabb89da75d5c41138d871
NullCascade <nullcascade@gmail.com>
2018-03-30 00:44:06 -0700

    Add TES3::Sound. Structure not fully mapped.

---
+++ MWSE/TES3Sound.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "TES3Object.h"
+
+namespace TES3 {
+	struct Sound : BaseObject {
+		char field_10;
+		char id[32]; // 0x11
+		char filename[32]; // 0x31
+		char unknown_0x51[3]; // Padding.
+		int unknown_0x54;
+	};
+	static_assert(sizeof(Sound) == 0x58, "TES3::Sound failed size validation");
+}
