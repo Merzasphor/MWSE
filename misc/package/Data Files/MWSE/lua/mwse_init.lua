ad6ccd78f3626d6e6bcc78bfe89f1e0f4ea3f1d8
NullCascade <nullcascade@gmail.com>
2018-03-31 20:09:55 -0700

    Add rxi's json library from https://github.com/rxi/json.lua

--- misc/package/Data Files/MWSE/lua/mwse_init.lua
+++ misc/package/Data Files/MWSE/lua/mwse_init.lua
@@ -4,4 +4,7 @@ package.path = "./Data Files/MWSE/lua/libs/?.lua;./Data Files/MWSE/lua/?.lua;"
 -- Load tes3 global.
 _G.tes3 = require("tes3.init")
 
+-- Other global libraries.
+_G.json = require("json")
+
 print("MWSE Lua interface initialized.")
