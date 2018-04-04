a2c8581d2f6c50018d812734fb8200769217bbc5
NullCascade <nullcascade@gmail.com>
2018-04-03 21:46:18 -0700

    Add mwscript.* calls for aiTravel, drop, equip, explodeSpell, hasItemEquipped, getItemCount, getSpellEffects, position, scriptRunning, setLevel, startCombat, startScript, and stopScript.

--- MWSE/ScriptUtilLua.cpp
+++ MWSE/ScriptUtilLua.cpp
@@ -13,7 +13,7 @@ namespace mwse {
 	namespace lua {
 		TES3::Script* getScriptParam(sol::table params) {
 			TES3::Script* script = NULL;
-			sol::object maybeScript = params["script"];
+			sol::object maybeScript = params["_script"];
 			if (maybeScript.valid() && maybeScript.is<TES3::Script*>()) {
 				script = maybeScript.as<TES3::Script*>();
 			}
@@ -123,6 +123,174 @@ namespace mwse {
 				return true;
 			};
 
+			state["mwscript"]["aiTravel"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve X.
+				sol::object xParam = params["x"];
+				float x = 0.0f;
+				if (xParam.is<double>()) {
+					x = xParam.as<double>();
+				}
+
+				// Resolve Y.
+				sol::object yParam = params["y"];
+				float y = 0.0f;
+				if (yParam.is<double>()) {
+					y = yParam.as<double>();
+				}
+
+				// Resolve Z.
+				sol::object zParam = params["z"];
+				float z = 0.0f;
+				if (zParam.is<double>()) {
+					z = zParam.as<double>();
+				}
+
+				mwscript::AITravel(script, reference, x, y, z);
+				return true;
+			};
+
+			state["mwscript"]["drop"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve item.
+				sol::object itemParam = params["item"];
+				TES3::BaseObject* item = NULL;
+				if (itemParam.is<std::string>()) {
+					item = tes3::getTemplate(itemParam.as<std::string>());
+				}
+				else if (itemParam.is<TES3::BaseObject*>()) {
+					item = itemParam.as<TES3::BaseObject*>();
+				}
+
+				if (item == NULL) {
+					return false;
+				}
+
+				// Resolve count.
+				sol::object countParam = params["count"];
+				long count = 1;
+				if (countParam.is<double>()) {
+					count = countParam.as<double>();
+				}
+
+				mwscript::Drop(script, reference, item, count);
+				return true;
+			};
+
+			state["mwscript"]["equip"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve item.
+				sol::object itemParam = params["item"];
+				TES3::BaseObject* item = NULL;
+				if (itemParam.is<std::string>()) {
+					item = tes3::getTemplate(itemParam.as<std::string>());
+				}
+				else if (itemParam.is<TES3::BaseObject*>()) {
+					item = itemParam.as<TES3::BaseObject*>();
+				}
+
+				if (item == NULL) {
+					return false;
+				}
+
+				mwscript::Equip(script, reference, item);
+				return true;
+			};
+
+			state["mwscript"]["explodeSpell"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve spell.
+				sol::object spellParam = params["spell"];
+				TES3::Spell* spell = NULL;
+				if (spellParam.is<std::string>()) {
+					spell = tes3::getObjectByID<TES3::Spell>(spellParam.as<std::string>(), TES3::ObjectType::Spell);
+				}
+				else if (spellParam.is<TES3::Spell*>()) {
+					spell = spellParam.as<TES3::Spell*>();
+				}
+
+				if (spell == NULL) {
+					return false;
+				}
+
+				mwscript::ExplodeSpell(script, reference, spell);
+				return true;
+			};
+
+			state["mwscript"]["hasItemEquipped"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve item.
+				sol::object itemParam = params["item"];
+				TES3::BaseObject* item = NULL;
+				if (itemParam.is<std::string>()) {
+					item = tes3::getTemplate(itemParam.as<std::string>());
+				}
+				else if (itemParam.is<TES3::BaseObject*>()) {
+					item = itemParam.as<TES3::BaseObject*>();
+				}
+
+				if (item == NULL) {
+					return false;
+				}
+
+				mwscript::HasItemEquipped(script, reference, item);
+				return true;
+			};
+
+			state["mwscript"]["getItemCount"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve item.
+				sol::object itemParam = params["item"];
+				TES3::BaseObject* item = NULL;
+				if (itemParam.is<std::string>()) {
+					item = tes3::getTemplate(itemParam.as<std::string>());
+				}
+				else if (itemParam.is<TES3::BaseObject*>()) {
+					item = itemParam.as<TES3::BaseObject*>();
+				}
+
+				if (item == NULL) {
+					return false;
+				}
+
+				mwscript::GetItemCount(script, reference, item);
+				return true;
+			};
+
+			state["mwscript"]["getSpellEffects"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve spell.
+				sol::object spellParam = params["spell"];
+				TES3::Spell* spell = NULL;
+				if (spellParam.is<std::string>()) {
+					spell = tes3::getObjectByID<TES3::Spell>(spellParam.as<std::string>(), TES3::ObjectType::Spell);
+				}
+				else if (spellParam.is<TES3::Spell*>()) {
+					spell = spellParam.as<TES3::Spell*>();
+				}
+
+				if (spell == NULL) {
+					return false;
+				}
+
+				mwscript::GetSpellEffects(script, reference, spell);
+				return true;
+			};
+
 			state["mwscript"]["placeAtPC"] = [](sol::table params) -> TES3::Reference* {
 				TES3::Script* script = getScriptParam(params);
 				TES3::Reference* reference = getReferenceParam(params);
@@ -166,11 +334,47 @@ namespace mwse {
 				return mwscript::lastCreatedPlaceAtPCReference;
 			};
 
-			state["mwscript"]["positionCell"] = [](sol::table params) {
+			state["mwscript"]["position"] = [](sol::table params) {
 				TES3::Script* script = getScriptParam(params);
 				TES3::Reference* reference = getReferenceParam(params);
 
 				// Resolve X.
+				sol::object xParam = params["x"];
+				float x = 0.0f;
+				if (xParam.is<double>()) {
+					x = xParam.as<double>();
+				}
+
+				// Resolve Y.
+				sol::object yParam = params["y"];
+				float y = 0.0f;
+				if (yParam.is<double>()) {
+					y = yParam.as<double>();
+				}
+
+				// Resolve Z.
+				sol::object zParam = params["z"];
+				float z = 0.0f;
+				if (zParam.is<double>()) {
+					z = zParam.as<double>();
+				}
+
+				// Resolve rotation.
+				sol::object rotationParam = params["rotation"];
+				float rotation = 0.0f;
+				if (rotationParam.is<double>()) {
+					rotation = rotationParam.as<double>();
+				}
+
+				mwscript::Position(script, reference, x, y, z, rotation);
+				return true;
+			};
+
+			state["mwscript"]["positionCell"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve cell.
 				sol::object cellParam = params["cell"];
 				std::string cell;
 				if (cellParam.is<std::string>()) {
@@ -261,6 +465,111 @@ namespace mwse {
 				}
 
 				mwscript::RemoveSpell(script, reference, spell);
+				return true;
+			};
+
+			state["mwscript"]["scriptRunning"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve script.
+				sol::object scriptParam = params["script"];
+				TES3::Script* targetScript = NULL;
+				if (scriptParam.is<std::string>()) {
+					targetScript = tes3::getScript(scriptParam.as<std::string>().c_str());
+				}
+				else if (scriptParam.is<TES3::Script*>()) {
+					targetScript = scriptParam.as<TES3::Script*>();
+				}
+
+				if (targetScript == NULL) {
+					return false;
+				}
+
+				return mwscript::ScriptRunning(script, targetScript);
+			};
+
+			state["mwscript"]["setLevel"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve level.
+				sol::object levelParam = params["level"];
+				short level = 0;
+				if (levelParam.is<double>()) {
+					level = levelParam.as<double>();
+				}
+				else {
+					return false;
+				}
+
+				mwscript::SetLevel(script, reference, level);
+				return true;
+			};
+
+			state["mwscript"]["startCombat"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve target.
+				sol::object targetParam = params["target"];
+				TES3::Reference* target = NULL;
+				if (targetParam.is<std::string>()) {
+					target = tes3::getReference(targetParam.as<std::string>().c_str());
+				}
+				else if (targetParam.is<TES3::Reference*>()) {
+					target = targetParam.as<TES3::Reference*>();
+				}
+
+				if (target == NULL) {
+					return false;
+				}
+
+				mwscript::StartCombat(script, reference, target);
+				return true;
+			};
+
+			state["mwscript"]["startScript"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve script.
+				sol::object scriptParam = params["script"];
+				TES3::Script* targetScript = NULL;
+				if (scriptParam.is<std::string>()) {
+					targetScript = tes3::getScript(scriptParam.as<std::string>().c_str());
+				}
+				else if (scriptParam.is<TES3::Script*>()) {
+					targetScript = scriptParam.as<TES3::Script*>();
+				}
+
+				if (targetScript == NULL) {
+					return false;
+				}
+
+				mwscript::StartScript(script, reference, targetScript);
+				return true;
+			};
+
+			state["mwscript"]["stopScript"] = [](sol::table params) {
+				TES3::Script* script = getScriptParam(params);
+				TES3::Reference* reference = getReferenceParam(params);
+
+				// Resolve script.
+				sol::object scriptParam = params["script"];
+				TES3::Script* targetScript = NULL;
+				if (scriptParam.is<std::string>()) {
+					targetScript = tes3::getScript(scriptParam.as<std::string>().c_str());
+				}
+				else if (scriptParam.is<TES3::Script*>()) {
+					targetScript = scriptParam.as<TES3::Script*>();
+				}
+
+				if (targetScript == NULL) {
+					return false;
+				}
+
+				mwscript::StartScript(script, reference, targetScript);
 				return true;
 			};
 
