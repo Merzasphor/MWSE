19bc8e6e301215892674c5398787c7c8d30f4f43
NullCascade <nullcascade@gmail.com>
2018-04-09 01:22:12 -0700

    Lua: Add bindings to various templated iterator types.

--- MWSE/TES3CollectionsLua.cpp
+++ MWSE/TES3CollectionsLua.cpp
@@ -3,11 +3,24 @@
 #include "LuaManager.h"
 
 #include "TES3Collections.h"
+#include "TES3Faction.h"
 #include "TES3Inventory.h"
+#include "TES3Reference.h"
 
 namespace mwse {
 	namespace lua {
 		void bindTES3Collections() {
+			// Iterator bindings.
+			bindIterator<TES3::EquipmentStack>("TES3EquipmentStackIterator");
+			bindIterator<TES3::Reference>("TES3ReferenceIterator");
+			bindIterator<TES3::Faction::ReactionNode>("TES3ReactionNodeIterator");
+			bindIterator<TES3::ItemStack>("TES3ItemStackIterator");
+
+			// Generic TES3::Object iterators. As above, but the result data will be
+			// reinterpreted at runtime.
+			bindGenericObjectIterator<TES3::MobileActor>("TES3MobileActorIterator");
+
+			// TArray bindings.
 			bindTArray<TES3::ItemVariables>("TES3ItemVariablesTArray");
 		}
 	}
--- MWSE/TES3CollectionsLua.h
+++ MWSE/TES3CollectionsLua.h
@@ -1,7 +1,63 @@
 #pragma once
 
+#include "TES3Collections.h"
+
+#include "LuaUtil.h"
+
+#include <cstddef>
+
 namespace mwse {
 	namespace lua {
+		template <typename T>
+		void bindGenericObjectIterator(const char* name) {
+			sol::state& state = LuaManager::getInstance().getState();
+
+			state.new_usertype<TES3::Iterator<T>>(name,
+				// Disable construction of this type.
+				"new", sol::no_constructor,
+
+				//
+				// Meta functions.
+				//
+
+				sol::meta_function::index, [](TES3::Iterator<T>& self, int index)
+			{
+				TES3::IteratorNode<T>* node = self.head;
+				for (int i = 0; i < index; i++) {
+					node = node->next;
+				}
+				return makeLuaObject(node->data);
+			},
+				sol::meta_function::length, [](TES3::Iterator<T>& self) { return self.size; }
+
+			);
+		}
+
+		template <typename T>
+		void bindIterator(const char* name) {
+			sol::state& state = LuaManager::getInstance().getState();
+
+			state.new_usertype<TES3::Iterator<T>>(name,
+				// Disable construction of this type.
+				"new", sol::no_constructor,
+
+				//
+				// Meta functions.
+				//
+
+				sol::meta_function::index, [](TES3::Iterator<T>& self, int index)
+			{
+				TES3::IteratorNode<T>* node = self.head;
+				for (int i = 0; i < index; i++) {
+					node = node->next;
+				}
+				return node->data;
+			},
+				sol::meta_function::length, [](TES3::Iterator<T>& self) { return self.size; }
+
+			);
+		}
+
 		template <typename T>
 		void bindTArray(const char* name) {
 			LuaManager::getInstance().getState().new_usertype<TES3::TArray<T>>(name,
