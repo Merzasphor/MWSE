7f9b7f2302f06db24d6b577e55e2a2303b4ae1a7
NullCascade <nullcascade@gmail.com>
2018-04-14 17:27:38 -0700

    Lua: Lowercase first letter in the tes3.* constants.

--- misc/package/Data Files/MWSE/lua/tes3/init.lua
+++ misc/package/Data Files/MWSE/lua/tes3/init.lua
@@ -1,21 +1,21 @@
 local tes3 = {}
 
 -- Game constants.
-tes3.ApparatusType = require("tes3.ApparatusType")
-tes3.AttachmentType = require("tes3.AttachmentType")
-tes3.Attribute = require("tes3.Attribute")
-tes3.AttributeName = require("tes3.AttributeName")
-tes3.Effect = require("tes3.Effect")
-tes3.EnchantmentType = require("tes3.EnchantmentType")
-tes3.ObjectType = require("tes3.ObjectType")
-tes3.Skill = require("tes3.Skill")
-tes3.SkillName = require("tes3.SkillName")
-tes3.SkillType = require("tes3.SkillType")
-tes3.Specialization = require("tes3.Specialization")
-tes3.SpecializationName = require("tes3.SpecializationName")
-tes3.SpellOrigin = require("tes3.SpellOrigin")
-tes3.SpellType = require("tes3.SpellType")
-tes3.WeaponType = require("tes3.WeaponType")
+tes3.apparatusType = require("tes3.ApparatusType")
+tes3.attachmentType = require("tes3.AttachmentType")
+tes3.attribute = require("tes3.Attribute")
+tes3.attributeName = require("tes3.AttributeName")
+tes3.effect = require("tes3.Effect")
+tes3.enchantmentType = require("tes3.EnchantmentType")
+tes3.objectType = require("tes3.ObjectType")
+tes3.skill = require("tes3.Skill")
+tes3.skillName = require("tes3.SkillName")
+tes3.skillType = require("tes3.SkillType")
+tes3.specialization = require("tes3.Specialization")
+tes3.specializationName = require("tes3.SpecializationName")
+tes3.spellOrigin = require("tes3.SpellOrigin")
+tes3.spellType = require("tes3.SpellType")
+tes3.weaponType = require("tes3.WeaponType")
 
 -------------------------------------------------
 -- Extend table API
