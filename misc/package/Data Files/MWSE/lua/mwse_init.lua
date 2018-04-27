2a046d8c2ac6c277f221468c128dde70ad001e37
NullCascade <nullcascade@gmail.com>
2018-04-26 19:54:29 -0700

    Lua: Require json globally.

--- misc/package/Data Files/MWSE/lua/mwse_init.lua
+++ misc/package/Data Files/MWSE/lua/mwse_init.lua
@@ -8,6 +8,7 @@ package.path = "./Data Files/MWSE/lua/?.lua;"
 _G.tes3 = require("mwse.tes3.init")
 _G.event = require("mwse.event")
 _G.timer = require("mwse.timer")
+_G.json = require("json")
 
 -------------------------------------------------
 -- Extend API: table
