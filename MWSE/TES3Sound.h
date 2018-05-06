49cbf80d569e625db305720b7a0a1afb5b84c966
NullCascade <nullcascade@gmail.com>
2018-05-05 18:36:34 -0700

    Fix up some unknown sound data.

--- MWSE/TES3Sound.h
+++ MWSE/TES3Sound.h
@@ -7,7 +7,9 @@ namespace TES3 {
 		char field_10;
 		char id[32]; // 0x11
 		char filename[32]; // 0x31
-		char unknown_0x51[3]; // Padding.
+		char unknown_0x51;
+		char unknown_0x52;
+		char unknown_0x53;
 		int unknown_0x54;
 	};
 	static_assert(sizeof(Sound) == 0x58, "TES3::Sound failed size validation");
