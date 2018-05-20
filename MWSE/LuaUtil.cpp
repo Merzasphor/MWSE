8eb0d810619fa9c02029d8063373b1fe871aa551
NullCascade <nullcascade@gmail.com>
2018-05-19 23:37:12 -0700

    Fix broken userdata mapping from when I added caching. God I'm dumb.

--- MWSE/LuaUtil.cpp
+++ MWSE/LuaUtil.cpp
@@ -165,78 +165,113 @@ namespace mwse {
 			switch (object->objectType) {
 			case TES3::ObjectType::Activator:
 				result = sol::make_object(state, reinterpret_cast<TES3::Activator*>(object));
+				break;
 			case TES3::ObjectType::Alchemy:
 				result = sol::make_object(state, reinterpret_cast<TES3::Alchemy*>(object));
+				break;
 			case TES3::ObjectType::Apparatus:
 				result = sol::make_object(state, reinterpret_cast<TES3::Apparatus*>(object));
+				break;
 			case TES3::ObjectType::Armor:
 				result = sol::make_object(state, reinterpret_cast<TES3::Armor*>(object));
+				break;
 			case TES3::ObjectType::Book:
 				result = sol::make_object(state, reinterpret_cast<TES3::Book*>(object));
+				break;
 			case TES3::ObjectType::Cell:
 				result = sol::make_object(state, reinterpret_cast<TES3::Cell*>(object));
+				break;
 			case TES3::ObjectType::Class:
 				result = sol::make_object(state, reinterpret_cast<TES3::Class*>(object));
+				break;
 			case TES3::ObjectType::Clothing:
 				result = sol::make_object(state, reinterpret_cast<TES3::Clothing*>(object));
+				break;
 			case TES3::ObjectType::Container:
+			{
 				if (reinterpret_cast<TES3::Actor*>(object)->actorFlags & TES3::ActorFlag::IsBase) {
 					result = sol::make_object(state, reinterpret_cast<TES3::Container*>(object));
 				}
 				else {
 					result = sol::make_object(state, reinterpret_cast<TES3::ContainerInstance*>(object));
 				}
+			}
+			break;
 			case TES3::ObjectType::Creature:
+			{
 				if (reinterpret_cast<TES3::Actor*>(object)->actorFlags & TES3::ActorFlag::IsBase) {
 					result = sol::make_object(state, reinterpret_cast<TES3::Creature*>(object));
 				}
 				else {
 					result = sol::make_object(state, reinterpret_cast<TES3::CreatureInstance*>(object));
 				}
+			}
+			break;
 			case TES3::ObjectType::Door:
 				result = sol::make_object(state, reinterpret_cast<TES3::Door*>(object));
+				break;
 			case TES3::ObjectType::Enchantment:
 				result = sol::make_object(state, reinterpret_cast<TES3::Enchantment*>(object));
+				break;
 			case TES3::ObjectType::Faction:
 				result = sol::make_object(state, reinterpret_cast<TES3::Faction*>(object));
+				break;
 			case TES3::ObjectType::Global:
 				result = sol::make_object(state, reinterpret_cast<TES3::GlobalVariable*>(object));
+				break;
 			case TES3::ObjectType::GameSetting:
 				result = sol::make_object(state, reinterpret_cast<TES3::GameSetting*>(object));
+				break;
 			case TES3::ObjectType::Ingredient:
 				result = sol::make_object(state, reinterpret_cast<TES3::Ingredient*>(object));
+				break;
 			case TES3::ObjectType::Light:
 				result = sol::make_object(state, reinterpret_cast<TES3::Light*>(object));
+				break;
 			case TES3::ObjectType::Lockpick:
 				result = sol::make_object(state, reinterpret_cast<TES3::Lockpick*>(object));
+				break;
 			case TES3::ObjectType::Misc:
 				result = sol::make_object(state, reinterpret_cast<TES3::Misc*>(object));
+				break;
 			case TES3::ObjectType::NPC:
+			{
 				if (reinterpret_cast<TES3::Actor*>(object)->actorFlags & TES3::ActorFlag::IsBase) {
 					result = sol::make_object(state, reinterpret_cast<TES3::NPC*>(object));
 				}
 				else {
 					result = sol::make_object(state, reinterpret_cast<TES3::NPCInstance*>(object));
 				}
+			}
+			break;
 			case TES3::ObjectType::Probe:
 				result = sol::make_object(state, reinterpret_cast<TES3::Probe*>(object));
+				break;
 			case TES3::ObjectType::Race:
 				result = sol::make_object(state, reinterpret_cast<TES3::Race*>(object));
+				break;
 			case TES3::ObjectType::Reference:
 				result = sol::make_object(state, reinterpret_cast<TES3::Reference*>(object));
+				break;
 			case TES3::ObjectType::Repair:
 				result = sol::make_object(state, reinterpret_cast<TES3::RepairTool*>(object));
+				break;
 			case TES3::ObjectType::Script:
 				result = sol::make_object(state, reinterpret_cast<TES3::Script*>(object));
+				break;
 			case TES3::ObjectType::Skill:
 				result = sol::make_object(state, reinterpret_cast<TES3::Skill*>(object));
+				break;
 			case TES3::ObjectType::Spell:
 				result = sol::make_object(state, reinterpret_cast<TES3::Spell*>(object));
+				break;
 			case TES3::ObjectType::Static:
 				result = sol::make_object(state, reinterpret_cast<TES3::Static*>(object));
+				break;
 			case TES3::ObjectType::Ammo:
 			case TES3::ObjectType::Weapon:
 				result = sol::make_object(state, reinterpret_cast<TES3::Weapon*>(object));
+				break;
 			}
 
 			// Insert the object into cache.
@@ -265,12 +300,16 @@ namespace mwse {
 			switch ((unsigned int)object->vTable.mobileObject) {
 			case TES3_vTable_MobileCreature:
 				result = sol::make_object(state, reinterpret_cast<TES3::MobileCreature*>(object));
+				break;
 			case TES3_vTable_MobileNPC:
 				result = sol::make_object(state, reinterpret_cast<TES3::MobileNPC*>(object));
+				break;
 			case TES3_vTable_MobilePlayer:
 				result = sol::make_object(state, reinterpret_cast<TES3::MobilePlayer*>(object));
+				break;
 			case TES3_vTable_MobileProjectile:
 				result = sol::make_object(state, reinterpret_cast<TES3::MobileProjectile*>(object));
+				break;
 			}
 
 			// Insert the object into cache.
