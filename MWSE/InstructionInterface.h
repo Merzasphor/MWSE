0a79750e97c5ac2f532540ad4a3b1a8b198ee5b1
NullCascade <nullcascade@gmail.com>
2018-05-26 18:43:03 -0700

    Nonsense whitespace change.

--- MWSE/InstructionInterface.h
+++ MWSE/InstructionInterface.h
@@ -27,8 +27,8 @@ namespace mwse {
     class InstructionInterface_t {
         public:
             inline OpCode::OpCode_t getOpCode() const { return opcode; }
-            virtual float execute(VMExecuteInterface &virtualMachine) =0;		//added 'virtual' so the linker wouldn't complain, added the '=0' back in.
-            virtual void loadParameters(VMExecuteInterface &virtualMachine) =0;	//same here
+            virtual float execute(VMExecuteInterface &virtualMachine) = 0;		//added 'virtual' so the linker wouldn't complain, added the '=0' back in.
+            virtual void loadParameters(VMExecuteInterface &virtualMachine) = 0;	//same here
         protected:
             InstructionInterface_t(const OpCode::OpCode_t ctor_opcode);
         private:
