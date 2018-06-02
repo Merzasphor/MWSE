f011f6c2a7df63e03bc1127a621d20c4193e2c4f
NullCascade <nullcascade@gmail.com>
2018-06-01 18:31:32 -0700

    Updater: Update assembly information.

--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Updater")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MWSE Auto-Updater")]
+[assembly: AssemblyDescription("Updates MWSE 2.x files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Updater")]
+[assembly: AssemblyCompany("MWSE Team.")]
+[assembly: AssemblyProduct("Morrowind Script Extender")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
