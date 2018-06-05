28e708e3f0ddd72990d51ed393f6444bb634864a
Greatness7 <Greatness7@gmail.com>
2018-06-05 18:35:34 -0400

    Lua: Allow event.unregister() on non-registered events to fail quietly.

--- misc/package/Data Files/MWSE/lua/mwse/event.lua
+++ misc/package/Data Files/MWSE/lua/mwse/event.lua
@@ -70,10 +70,10 @@ function this.unregister(eventType, callback, options)
 	local options = options or {}
 
 	local callbacks = getEventTable(eventType, options.filter)
-	if (not table.removevalue(callbacks, callback)) then
-		print("event.register: Attempted to unregister '" .. eventType .. "' event callback that wasn't registered.")
-		print(debug.traceback())
-	end
+	-- if (not table.removevalue(callbacks, callback)) then
+	-- 	print("event.register: Attempted to unregister '" .. eventType .. "' event callback that wasn't registered.")
+	-- 	print(debug.traceback())
+	-- end
 end
 
 function this.clear(eventType, filter)
