e3b4a91903fc849a6ca6a58188c8971a1b804a11
NullCascade <nullcascade@gmail.com>
2018-06-09 12:07:22 -0700

    Refactor lua bindings for TES3::Region. Add new region:randomizeWeather() function.

--- MWSE/TES3RegionLua.cpp
+++ MWSE/TES3RegionLua.cpp
@@ -10,43 +10,38 @@
 namespace mwse {
 	namespace lua {
 		void bindTES3Region() {
-			LuaManager::getInstance().getState().new_usertype<TES3::Region>("TES3Region",
-				// Disable construction of this type.
-				"new", sol::no_constructor,
+			// Get our lua state.
+			sol::state& state = LuaManager::getInstance().getState();
 
-				sol::base_classes, sol::bases<TES3::BaseObject>(),
+			// Start our usertype. We must finish this with state.set_usertype.
+			auto usertypeDefinition = state.create_simple_usertype<TES3::Region>();
+			usertypeDefinition.set("new", sol::no_constructor);
 
-				sol::meta_function::to_string, &TES3::Region::getObjectID,
+			// Define inheritance structures. These must be defined in order from top to bottom. The complete chain must be defined.
+			usertypeDefinition.set(sol::base_classes, sol::bases<TES3::BaseObject>());
 
-				//
-				// Properties.
-				//
+			// Basic property binding.
+			usertypeDefinition.set("name", sol::property([](TES3::Region& self) { return self.name; }));
+			usertypeDefinition.set("weatherChanceAsh", &TES3::Region::weatherChanceAsh);
+			usertypeDefinition.set("weatherChanceBlight", &TES3::Region::weatherChanceBlight);
+			usertypeDefinition.set("weatherChanceBlizzard", &TES3::Region::weatherChanceBlizzard);
+			usertypeDefinition.set("weatherChanceClear", &TES3::Region::weatherChanceClear);
+			usertypeDefinition.set("weatherChanceCloudy", &TES3::Region::weatherChanceCloudy);
+			usertypeDefinition.set("weatherChanceFoggy", &TES3::Region::weatherChanceFoggy);
+			usertypeDefinition.set("weatherChanceOvercast", &TES3::Region::weatherChanceOvercast);
+			usertypeDefinition.set("weatherChanceRain", &TES3::Region::weatherChanceRain);
+			usertypeDefinition.set("weatherChanceSnow", &TES3::Region::weatherChanceSnow);
+			usertypeDefinition.set("weatherChanceThunder", &TES3::Region::weatherChanceThunder);
 
-				"objectType", &TES3::Region::objectType,
+			// Access to other objects that need to be packaged.
+			usertypeDefinition.set("sleepCreature", sol::readonly_property([](TES3::Region& self) { return makeLuaObject(self.sleepCreature); }));
 
-				"id", sol::readonly_property(&TES3::Region::getObjectID),
-				"name", sol::property([](TES3::Region& self) { return self.name; }),
+			// Basic function binding.
+			usertypeDefinition.set("changeWeather", &TES3::Region::changeWeather);
+			usertypeDefinition.set("randomizeWeather", &TES3::Region::randomizeWeather);
 
-				"weatherChanceClear", &TES3::Region::weatherChanceClear,
-				"weatherChanceCloudy", &TES3::Region::weatherChanceCloudy,
-				"weatherChanceFoggy", &TES3::Region::weatherChanceFoggy,
-				"weatherChanceOvercast", &TES3::Region::weatherChanceOvercast,
-				"weatherChanceRain", &TES3::Region::weatherChanceRain,
-				"weatherChanceThunder", &TES3::Region::weatherChanceThunder,
-				"weatherChanceAsh", &TES3::Region::weatherChanceAsh,
-				"weatherChanceBlight", &TES3::Region::weatherChanceBlight,
-				"weatherChanceSnow", &TES3::Region::weatherChanceSnow,
-				"weatherChanceBlizzard", &TES3::Region::weatherChanceBlizzard,
-
-				"sleepCreature", sol::readonly_property([](TES3::Region& self) { return makeLuaObject(self.sleepCreature); }),
-
-				//
-				// Functions.
-				//
-
-				"changeWeather", &TES3::Region::changeWeather
-
-				);
+			// Finish up our usertype.
+			state.set_usertype("tes3region", usertypeDefinition);
 		}
 	}
 }
