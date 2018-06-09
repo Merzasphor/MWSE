7a0c091f5afb08ce8fc069d62d1c08873d147535
NullCascade <nullcascade@gmail.com>
2018-06-09 12:57:39 -0700

    Refactor lua binding for TES3::Skill.

--- MWSE/TES3SkillLua.cpp
+++ MWSE/TES3SkillLua.cpp
@@ -8,26 +8,26 @@
 namespace mwse {
 	namespace lua {
 		void bindTES3Skill() {
+			// Get our lua state.
 			sol::state& state = LuaManager::getInstance().getState();
 
-			state.new_usertype<TES3::Skill>("TES3Skill",
-				// Disable construction of this type.
-				"new", sol::no_constructor,
+			// Start our usertype. We must finish this with state.set_usertype.
+			auto usertypeDefinition = state.create_simple_usertype<TES3::Skill>();
+			usertypeDefinition.set("new", sol::no_constructor);
 
-				sol::base_classes, sol::bases<TES3::BaseObject>(),
+			// Define inheritance structures. These must be defined in order from top to bottom. The complete chain must be defined.
+			usertypeDefinition.set(sol::base_classes, sol::bases<TES3::BaseObject>());
 
-				sol::meta_function::to_string, &TES3::Skill::getObjectID,
+			// Basic property binding.
+			usertypeDefinition.set("id", sol::readonly_property(&TES3::Skill::skill));
+			usertypeDefinition.set("attribute", &TES3::Skill::governingAttribute);
+			usertypeDefinition.set("specialization", &TES3::Skill::specialization);
 
-				//
-				// Properties.
-				//
+			// Indirect bindings to unions and arrays.
+			usertypeDefinition.set("actions", sol::readonly_property([](TES3::Skill& self) { return std::ref(self.progressActions); }));
 
-				"id", &TES3::Skill::skill,
-				"attribute", &TES3::Skill::governingAttribute,
-				"specialization", &TES3::Skill::specialization,
-				"actions", sol::readonly_property([](TES3::Skill& self) { return std::ref(self.progressActions); })
-
-				);
+			// Finish up our usertype.
+			state.set_usertype("tes3skill", usertypeDefinition);
 		}
 	}
 }
