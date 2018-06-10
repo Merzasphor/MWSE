4fd57d7dbb550c11b7315c00a48f62f40692128a
NullCascade <nullcascade@gmail.com>
2018-06-10 00:14:59 -0700

    Refactor lua binding for TES3::UI::Block.

--- MWSE/TES3UIBlockLua.cpp
+++ MWSE/TES3UIBlockLua.cpp
@@ -8,22 +8,21 @@
 namespace mwse {
 	namespace lua {
 		void bindTES3UIBlock() {
-			LuaManager::getInstance().getState().new_usertype<TES3::UI::Block>("TES3UIBlock",
-				// Disable construction of this type.
-				"new", sol::no_constructor,
+			// Get our lua state.
+			sol::state& state = LuaManager::getInstance().getState();
 
-				//
-				// Properties.
-				//
+			// Start our usertype. We must finish this with state.set_usertype.
+			auto usertypeDefinition = state.create_simple_usertype<TES3::UI::Block>();
+			usertypeDefinition.set("new", sol::no_constructor);
 
-				"tag", sol::readonly_property(&TES3::UI::Block::tag),
+			// Basic property bindings.
+			usertypeDefinition.set("id", sol::readonly_property(&TES3::UI::Block::id));
+			usertypeDefinition.set("name", sol::readonly_property([](TES3::UI::Block& self) { return self.name.cString; }));
+			usertypeDefinition.set("parent", sol::readonly_property(&TES3::UI::Block::parent));
+			usertypeDefinition.set("tag", sol::readonly_property(&TES3::UI::Block::tag));
 
-				"id", sol::readonly_property(&TES3::UI::Block::id),
-				"name", sol::readonly_property([](TES3::UI::Block& self) { return self.name.cString; }),
-
-				"parent", sol::readonly_property(&TES3::UI::Block::parent)
-
-				);
+			// Finish up our usertype.
+			state.set_usertype("tes3uiBlock", usertypeDefinition);
 		}
 	}
 }
