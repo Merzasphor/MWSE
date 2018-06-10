9930f1c88160b13224f9b26761350f2069dd4741
NullCascade <nullcascade@gmail.com>
2018-06-10 01:12:43 -0700

    Refactor lua binding for TES3::Weather.

--- MWSE/TES3WeatherLua.cpp
+++ MWSE/TES3WeatherLua.cpp
@@ -8,49 +8,44 @@
 namespace mwse {
 	namespace lua {
 		void bindTES3Weather() {
+			// Get our lua state.
 			sol::state& state = LuaManager::getInstance().getState();
 
-			state.new_usertype<TES3::Weather>("TES3Weather",
-				// Disable construction of this type.
-				"new", sol::no_constructor,
+			// Start our usertype. We must finish this with state.set_usertype.
+			auto usertypeDefinition = state.create_simple_usertype<TES3::Weather>();
+			usertypeDefinition.set("new", sol::no_constructor);
 
-				//
-				// Properties.
-				//
+			// Basic property binding.
+			usertypeDefinition.set("ambientDayColor", &TES3::Weather::ambientDayCol);
+			usertypeDefinition.set("ambientNightColor", &TES3::Weather::ambientNightCol);
+			usertypeDefinition.set("ambientSunriseColor", &TES3::Weather::ambientSunriseCol);
+			usertypeDefinition.set("ambientSunsetColor", &TES3::Weather::ambientSunsetCol);
+			usertypeDefinition.set("cloudsMaxPercent", &TES3::Weather::cloudsMaxPercent);
+			usertypeDefinition.set("cloudsSpeed", &TES3::Weather::cloudsSpeed);
+			usertypeDefinition.set("cloudTexture", &TES3::Weather::texturePathCloud);
+			usertypeDefinition.set("fogDayColor", &TES3::Weather::fogDayCol);
+			usertypeDefinition.set("fogNightColor", &TES3::Weather::fogNightCol);
+			usertypeDefinition.set("fogSunriseColor", &TES3::Weather::fogSunriseCol);
+			usertypeDefinition.set("fogSunsetColor", &TES3::Weather::fogSunsetCol);
+			usertypeDefinition.set("glareView", &TES3::Weather::glareView);
+			usertypeDefinition.set("landFogDayDepth", &TES3::Weather::landFogDayDepth);
+			usertypeDefinition.set("landFogNightDepth", &TES3::Weather::landFogNightDepth);
+			usertypeDefinition.set("skyDayColor", &TES3::Weather::skyDayCol);
+			usertypeDefinition.set("skyNightColor", &TES3::Weather::skyNightCol);
+			usertypeDefinition.set("skySunriseColor", &TES3::Weather::skySunriseCol);
+			usertypeDefinition.set("skySunsetColor", &TES3::Weather::skySunsetCol);
+			usertypeDefinition.set("soundAmbientLoops", &TES3::Weather::soundIDAmbientLoop);
+			usertypeDefinition.set("sunDayColor", &TES3::Weather::sunDayCol);
+			usertypeDefinition.set("sundiscSunsetColor", &TES3::Weather::sundiscSunsetCol);
+			usertypeDefinition.set("sunNightColor", &TES3::Weather::sunNightCol);
+			usertypeDefinition.set("sunSunriseColor", &TES3::Weather::sunSunriseCol);
+			usertypeDefinition.set("sunSunsetColor", &TES3::Weather::sunSunsetCol);
+			usertypeDefinition.set("transitionDelta", &TES3::Weather::transitionDelta);
+			usertypeDefinition.set("underwaterSoundActive", &TES3::Weather::flagUnderwaterSoundActive);
+			usertypeDefinition.set("windSpeed", &TES3::Weather::windSpeed);
 
-				"transitionDelta", &TES3::Weather::transitionDelta,
-
-				"ambientSunriseColor", &TES3::Weather::ambientSunriseCol,
-				"ambientDayColor", &TES3::Weather::ambientDayCol,
-				"ambientSunsetColor", &TES3::Weather::ambientSunsetCol,
-				"ambientNightColor", &TES3::Weather::ambientNightCol,
-				"fogSunriseColor", &TES3::Weather::fogSunriseCol,
-				"fogDayColor", &TES3::Weather::fogDayCol,
-				"fogSunsetColor", &TES3::Weather::fogSunsetCol,
-				"fogNightColor", &TES3::Weather::fogNightCol,
-				"skySunriseColor", &TES3::Weather::skySunriseCol,
-				"skyDayColor", &TES3::Weather::skyDayCol,
-				"skySunsetColor", &TES3::Weather::skySunsetCol,
-				"skyNightColor", &TES3::Weather::skyNightCol,
-				"sunSunriseColor", &TES3::Weather::sunSunriseCol,
-				"sunDayColor", &TES3::Weather::sunDayCol,
-				"sunSunsetColor", &TES3::Weather::sunSunsetCol,
-				"sunNightColor", &TES3::Weather::sunNightCol,
-				"sundiscSunsetColor", &TES3::Weather::sundiscSunsetCol,
-
-				"glareView", &TES3::Weather::glareView,
-				"cloudsMaxPercent", &TES3::Weather::cloudsMaxPercent,
-				"landFogDayDepth", &TES3::Weather::landFogDayDepth,
-				"landFogNightDepth", &TES3::Weather::landFogNightDepth,
-				"cloudsSpeed", &TES3::Weather::cloudsSpeed,
-				"windSpeed", &TES3::Weather::windSpeed,
-
-				"cloudTexture", &TES3::Weather::texturePathCloud,
-
-				"underwaterSoundActive", &TES3::Weather::flagUnderwaterSoundActive,
-				"soundAmbientLoops", &TES3::Weather::soundIDAmbientLoop
-
-				);
+			// Finish up our usertype.
+			state.set_usertype("tes3weather", usertypeDefinition);
 		}
 	}
 }
