b569aa796f287871f5386e49706b1c803681c265
NullCascade <nullcascade@gmail.com>
2018-06-10 00:29:10 -0700

    Refactor lua binding for TES3::Vector2, TES3::Vector3, TES3::Vector4m and TES3::BoundingBox.

--- MWSE/TES3VectorsLua.cpp
+++ MWSE/TES3VectorsLua.cpp
@@ -10,70 +10,102 @@
 namespace mwse {
 	namespace lua {
 		void bindTES3Vectors() {
+			// Disable construction of this type.
 			sol::state& state = LuaManager::getInstance().getState();
 
-			state.new_usertype<TES3::Vector2>("TES3Vector2",
-				"new", sol::constructors<TES3::Vector2(), TES3::Vector2(float, float)>(),
-				"copy", [](TES3::Vector2& self) { return TES3::Vector2(self); },
-				"x", &TES3::Vector2::x,
-				"y", &TES3::Vector2::y
-			);
-
-			state.new_usertype<TES3::Vector3>("TES3Vector3",
-				"new", sol::constructors<TES3::Vector3(), TES3::Vector3(float, float, float)>(),
-				"copy", [](TES3::Vector3& self) { return TES3::Vector3(self); },
-
-				sol::meta_function::addition, &TES3::Vector3::operator+,
-				sol::meta_function::subtraction, &TES3::Vector3::operator-,
-				sol::meta_function::length, &TES3::Vector3::length,
-				sol::meta_function::to_string, [](TES3::Vector3& self)
+			// Binding for TES3::Vector2.
 			{
-				std::ostringstream ss;
-				ss << std::fixed << std::setprecision(2) << "<" << self.x << ", " << self.y << ", " << self.z << ">";
-				return ss.str();
-			},
+				// Start our usertype. We must finish this with state.set_usertype.
+				auto usertypeDefinition = state.create_simple_usertype<TES3::Vector2>();
+				usertypeDefinition.set("new", sol::constructors<TES3::Vector2(), TES3::Vector2(float, float)>());
 
-				//
-				// Properties.
-				//
+				// Basic property bindings.
+				usertypeDefinition.set("x", &TES3::Vector2::x);
+				usertypeDefinition.set("y", &TES3::Vector2::y);
 
-				"x", &TES3::Vector3::x,
-				"y", &TES3::Vector3::y,
-				"z", &TES3::Vector3::z,
+				// Basic function binding.
+				usertypeDefinition.set("copy", [](TES3::Vector2& self) { return TES3::Vector2(self); });
 
-				// This can also be used for colors, to expose it as rgb.
-				"r", &TES3::Vector3::x,
-				"g", &TES3::Vector3::y,
-				"b", &TES3::Vector3::z,
+				// Finish up our usertype.
+				state.set_usertype("tes3vector2", usertypeDefinition);
+			}
 
-				//
-				// Functions.
-				//
+			// Binding for TES3::Vector3.
+			{
+				// Start our usertype. We must finish this with state.set_usertype.
+				auto usertypeDefinition = state.create_simple_usertype<TES3::Vector3>();
+				usertypeDefinition.set("new", sol::constructors<TES3::Vector3(), TES3::Vector3(float, float, float)>());
 
-				"cross", &TES3::Vector3::crossProduct,
-				"dot", &TES3::Vector3::dotProduct,
-
-				"length", &TES3::Vector3::length,
-				"distance", &TES3::Vector3::distance,
-				"heightDifference", &TES3::Vector3::heightDifference,
-				"negate", &TES3::Vector3::negate
-
+				// Operator overloading.
+				usertypeDefinition.set(sol::meta_function::addition, &TES3::Vector3::operator+);
+				usertypeDefinition.set(sol::meta_function::subtraction, &TES3::Vector3::operator-);
+				usertypeDefinition.set(sol::meta_function::length, &TES3::Vector3::length);
+				usertypeDefinition.set(sol::meta_function::to_string, [](TES3::Vector3& self)
+				{
+					std::ostringstream ss;
+					ss << std::fixed << std::setprecision(2) << "<" << self.x << ", " << self.y << ", " << self.z << ">";
+					return ss.str();
+				}
 				);
 
-			state.new_usertype<TES3::Vector4>("TES3Vector4",
-				"new", sol::constructors<TES3::Vector4(), TES3::Vector4(float, float, float, float)>(),
-				"copy", [](TES3::Vector4& self) { return TES3::Vector4(self); },
-				"x", &TES3::Vector4::x,
-				"y", &TES3::Vector4::y,
-				"z", &TES3::Vector4::z,
-				"w", &TES3::Vector4::w
-				);
+				// Basic property bindings.
+				usertypeDefinition.set("x", &TES3::Vector3::x);
+				usertypeDefinition.set("y", &TES3::Vector3::y);
+				usertypeDefinition.set("z", &TES3::Vector3::z);
 
-			state.new_usertype<TES3::BoundingBox>("TES3BoundingBox",
-				"copy", [](TES3::BoundingBox& self) { return TES3::BoundingBox(self); },
-				"min", &TES3::BoundingBox::minimum,
-				"max", &TES3::BoundingBox::maximum
-				);
+				// These can also be used for RGB.
+				usertypeDefinition.set("r", &TES3::Vector3::x);
+				usertypeDefinition.set("g", &TES3::Vector3::y);
+				usertypeDefinition.set("b", &TES3::Vector3::z);
+
+				// Basic function binding.
+				usertypeDefinition.set("copy", [](TES3::Vector3& self) { return TES3::Vector3(self); });
+				usertypeDefinition.set("cross", &TES3::Vector3::crossProduct);
+				usertypeDefinition.set("distance", &TES3::Vector3::distance);
+				usertypeDefinition.set("dot", &TES3::Vector3::dotProduct);
+				usertypeDefinition.set("heightDifference", &TES3::Vector3::heightDifference);
+				usertypeDefinition.set("length", &TES3::Vector3::length);
+				usertypeDefinition.set("negate", &TES3::Vector3::negate);
+
+				// Finish up our usertype.
+				state.set_usertype("tes3vector3", usertypeDefinition);
+			}
+
+			// Binding for TES3::Vector4.
+			{
+				// Start our usertype. We must finish this with state.set_usertype.
+				auto usertypeDefinition = state.create_simple_usertype<TES3::Vector4>();
+				usertypeDefinition.set("new", sol::constructors<TES3::Vector4(), TES3::Vector4(float, float, float, float)>());
+
+				// Basic property bindings.
+				usertypeDefinition.set("x", &TES3::Vector4::x);
+				usertypeDefinition.set("y", &TES3::Vector4::y);
+				usertypeDefinition.set("z", &TES3::Vector4::z);
+				usertypeDefinition.set("w", &TES3::Vector4::w);
+
+				// Basic function binding.
+				usertypeDefinition.set("copy", [](TES3::Vector4& self) { return TES3::Vector4(self); });
+
+				// Finish up our usertype.
+				state.set_usertype("tes3vector4", usertypeDefinition);
+			}
+
+			// Binding for TES3::BoundingBox.
+			{
+				// Start our usertype. We must finish this with state.set_usertype.
+				auto usertypeDefinition = state.create_simple_usertype<TES3::BoundingBox>();
+				usertypeDefinition.set("new", sol::no_constructor);
+
+				// Basic property bindings.
+				usertypeDefinition.set("max", &TES3::BoundingBox::maximum);
+				usertypeDefinition.set("min", &TES3::BoundingBox::minimum);
+
+				// Basic function binding.
+				usertypeDefinition.set("copy", [](TES3::BoundingBox& self) { return TES3::BoundingBox(self); });
+
+				// Finish up our usertype.
+				state.set_usertype("tes3vector4", usertypeDefinition);
+			}
 		}
 	}
 }
