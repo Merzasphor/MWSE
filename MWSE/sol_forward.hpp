b090be31a2c006fbfea1fea76b9fa447c96ffd71
NullCascade <nullcascade@gmail.com>
2018-06-16 05:14:40 -0700

    Update sol2 from v2.20.2 to v2.20.3.

--- MWSE/sol_forward.hpp
+++ MWSE/sol_forward.hpp
@@ -20,8 +20,8 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // This file was generated with a script.
-// Generated 2018-05-22 19:42:19.843589 UTC
-// This header was generated with sol v2.20.2 (revision d67c5b7)
+// Generated 2018-06-16 11:58:58.882218 UTC
+// This header was generated with sol v2.20.3 (revision 968989b)
 // https://github.com/ThePhD/sol2
 
 #ifndef SOL_SINGLE_INCLUDE_FORWARD_HPP
@@ -221,6 +221,10 @@
 
 // end of sol/config.hpp
 
+// beginning of sol/config_setup.hpp
+
+// end of sol/config_setup.hpp
+
 // end of sol/feature_test.hpp
 
 namespace sol {
@@ -350,6 +354,11 @@ namespace sol {
 	struct protect_t;
 	template <typename F, typename... Filters>
 	struct filter_wrapper;
+
+	template <typename T>
+	struct usertype_traits;
+	template <typename T>
+	struct unique_usertype_traits;
 } // namespace sol
 
 // end of sol/forward.hpp
