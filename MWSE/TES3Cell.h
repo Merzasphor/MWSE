eecdcf2c690b883a4c3caec42f2fb189fc14a912
NullCascade <nullcascade@gmail.com>
2018-06-26 22:12:23 -0700

    TES3::Cell: Reuse CellExteriorData structure. Add offsets to comments. Fix type for water level and fog density.

--- MWSE/TES3Cell.h
+++ MWSE/TES3Cell.h
@@ -16,35 +16,32 @@ namespace TES3 {
 	}
 
 	struct CellExteriorData {
-		int size;
-		Cell* cell;
-		int gridX;
-		int gridY;
+		int size; // 0x0
+		Cell* cell; // 0x4
+		int gridX; // 0x8
+		int gridY; // 0xC
 	};
+	static_assert(sizeof(CellExteriorData) == 0x10, "TES3::CellExteriorData failed size validation");
 	
 	struct PackedColor {
-		unsigned char r;
-		unsigned char g;
-		unsigned char b;
-		unsigned char a;
+		unsigned char r; // 0x0
+		unsigned char g; // 0x1
+		unsigned char b; // 0x2
+		unsigned char a; // 0x3
 	};
+	static_assert(sizeof(PackedColor) == 0x4, "TES3::PackedColor failed size validation");
 
 	struct Cell : BaseObject {
 		char * name; // 0x10
 		void * unknown_0x14;
 		int cellFlags; // 0x18
 		union {
-			struct {
-				int unknown_0x0;
-				int unknown_0x4;
-				int gridX; // 0x8
-				int gridY; // 0xC
-			} exterior;
+			CellExteriorData exterior;
 			struct {
 				PackedColor ambientColor; // 0x0
 				PackedColor sunColor; // 0x4
 				PackedColor fogColor; // 0x8
-				int fogDensity; // 0xC
+				float fogDensity; // 0xC
 			} interior;
 		} VariantData; // 0x1C
 		void * unknown_0x2C;
@@ -53,16 +50,16 @@ namespace TES3 {
 		void * unknown_0x50; // Pointer list?
 		int unknown_0x54;
 		ReferenceList statics; // 0x58
-		Iterator<Reference> moveReferences;
+		Iterator<Reference> moveReferences; // 0x68
 		int unknown_0x7C;
 		int unknown_0x80;
 		int unknown_0x84;
 		int unknown_0x88;
-		void * pathGrid;
+		void * pathGrid; // 0x8C
 		union {
-			int waterLevel;
+			float waterLevel;
 			Region * region;
-		} waterLevelOrRegion;
+		} waterLevelOrRegion; // 0x90
 
 		//
 		// Other related this-call functions.
