6ce4c04018f88186bb385b2c1eb8021af22b0f5a
NullCascade <nullcascade@gmail.com>
2018-06-29 15:30:30 -0700

    Tighten up weather lua access. Add basic docs for weather objects.

--- MWSE/TES3WeatherLua.cpp
+++ MWSE/TES3WeatherLua.cpp
@@ -16,31 +16,34 @@ namespace mwse {
 			usertypeDefinition.set("new", sol::no_constructor);
 
 			// Basic property binding.
-			usertypeDefinition.set("ambientDayColor", &TES3::Weather::ambientDayCol);
-			usertypeDefinition.set("ambientNightColor", &TES3::Weather::ambientNightCol);
-			usertypeDefinition.set("ambientSunriseColor", &TES3::Weather::ambientSunriseCol);
-			usertypeDefinition.set("ambientSunsetColor", &TES3::Weather::ambientSunsetCol);
+			usertypeDefinition.set("ambientDayColor", sol::readonly_property(&TES3::Weather::ambientDayCol));
+			usertypeDefinition.set("ambientNightColor", sol::readonly_property(&TES3::Weather::ambientNightCol));
+			usertypeDefinition.set("ambientSunriseColor", sol::readonly_property(&TES3::Weather::ambientSunriseCol));
+			usertypeDefinition.set("ambientSunsetColor", sol::readonly_property(&TES3::Weather::ambientSunsetCol));
 			usertypeDefinition.set("cloudsMaxPercent", &TES3::Weather::cloudsMaxPercent);
 			usertypeDefinition.set("cloudsSpeed", &TES3::Weather::cloudsSpeed);
-			usertypeDefinition.set("cloudTexture", &TES3::Weather::texturePathCloud);
-			usertypeDefinition.set("fogDayColor", &TES3::Weather::fogDayCol);
-			usertypeDefinition.set("fogNightColor", &TES3::Weather::fogNightCol);
-			usertypeDefinition.set("fogSunriseColor", &TES3::Weather::fogSunriseCol);
-			usertypeDefinition.set("fogSunsetColor", &TES3::Weather::fogSunsetCol);
+			usertypeDefinition.set("cloudTexture", sol::property(
+				[](TES3::Weather& self) { return self.texturePathCloud; },
+				[](TES3::Weather& self, const char* value) { tes3::setDataString(&self.texturePathCloud, value); }
+			));
+			usertypeDefinition.set("fogDayColor", sol::readonly_property(&TES3::Weather::fogDayCol));
+			usertypeDefinition.set("fogNightColor", sol::readonly_property(&TES3::Weather::fogNightCol));
+			usertypeDefinition.set("fogSunriseColor", sol::readonly_property(&TES3::Weather::fogSunriseCol));
+			usertypeDefinition.set("fogSunsetColor", sol::readonly_property(&TES3::Weather::fogSunsetCol));
 			usertypeDefinition.set("glareView", &TES3::Weather::glareView);
 			usertypeDefinition.set("index", sol::readonly_property(&TES3::Weather::index));
 			usertypeDefinition.set("landFogDayDepth", &TES3::Weather::landFogDayDepth);
 			usertypeDefinition.set("landFogNightDepth", &TES3::Weather::landFogNightDepth);
-			usertypeDefinition.set("skyDayColor", &TES3::Weather::skyDayCol);
-			usertypeDefinition.set("skyNightColor", &TES3::Weather::skyNightCol);
-			usertypeDefinition.set("skySunriseColor", &TES3::Weather::skySunriseCol);
-			usertypeDefinition.set("skySunsetColor", &TES3::Weather::skySunsetCol);
+			usertypeDefinition.set("skyDayColor", sol::readonly_property(&TES3::Weather::skyDayCol));
+			usertypeDefinition.set("skyNightColor", sol::readonly_property(&TES3::Weather::skyNightCol));
+			usertypeDefinition.set("skySunriseColor", sol::readonly_property(&TES3::Weather::skySunriseCol));
+			usertypeDefinition.set("skySunsetColor", sol::readonly_property(&TES3::Weather::skySunsetCol));
 			usertypeDefinition.set("soundAmbientLoops", &TES3::Weather::soundIDAmbientLoop);
-			usertypeDefinition.set("sunDayColor", &TES3::Weather::sunDayCol);
-			usertypeDefinition.set("sundiscSunsetColor", &TES3::Weather::sundiscSunsetCol);
-			usertypeDefinition.set("sunNightColor", &TES3::Weather::sunNightCol);
-			usertypeDefinition.set("sunSunriseColor", &TES3::Weather::sunSunriseCol);
-			usertypeDefinition.set("sunSunsetColor", &TES3::Weather::sunSunsetCol);
+			usertypeDefinition.set("sunDayColor", sol::readonly_property(&TES3::Weather::sunDayCol));
+			usertypeDefinition.set("sundiscSunsetColor", sol::readonly_property(&TES3::Weather::sundiscSunsetCol));
+			usertypeDefinition.set("sunNightColor", sol::readonly_property(&TES3::Weather::sunNightCol));
+			usertypeDefinition.set("sunSunriseColor", sol::readonly_property(&TES3::Weather::sunSunriseCol));
+			usertypeDefinition.set("sunSunsetColor", sol::readonly_property(&TES3::Weather::sunSunsetCol));
 			usertypeDefinition.set("transitionDelta", &TES3::Weather::transitionDelta);
 			usertypeDefinition.set("underwaterSoundActive", &TES3::Weather::flagUnderwaterSoundActive);
 			usertypeDefinition.set("windSpeed", &TES3::Weather::windSpeed);
---
+++ docs/source/lua/type/tes3/weather.rst
@@ -0,0 +1,102 @@
+
+Weather
+====================================================================================================
+
+A weather is attached to a region and provides shifts to color and sounds to accentuate the feel of the area.
+
+
+Properties
+----------------------------------------------------------------------------------------------------
+
+**ambientDayColor** (`tes3vector3`_, read-only)
+    The weather's ambient color during the day.
+
+**ambientNightColor** (`tes3vector3`_, read-only)
+    The weather's ambient color during the night.
+
+**ambientSunriseColor** (`tes3vector3`_, read-only)
+    The weather's ambient color during sunrise.
+
+**ambientSunsetColor** (`tes3vector3`_, read-only)
+    The weather's ambient color during sunset.
+
+**cloudsMaxPercent** (`number`_)
+    The maximum percentage used for cloud cover.
+
+**cloudsSpeed** (`number`_)
+    The movement speed for clouds.
+
+**cloudTexture** (`string`_)
+    The texture used for the clouds.
+
+**fogDayColor** (`tes3vector3`_, read-only)
+    The weather's fog color during the day.
+
+**fogNightColor** (`tes3vector3`_, read-only)
+    The weather's fog color during the night.
+
+**fogSunriseColor** (`tes3vector3`_, read-only)
+    The weather's fog color during sunrise.
+
+**fogSunsetColor** (`tes3vector3`_, read-only)
+    The weather's fog color during sunset.
+
+**glareView** (`number`_)
+    Requires research.
+
+**index** (`number`_, read-only)
+    The object's unique id.
+
+**landFogDayDepth** (`number`_)
+    Requires research.
+
+**landFogNightDepth** (`number`_)
+    Requires research.
+
+**skyDayColor** (`tes3vector3`_, read-only)
+    The weather's sky color during the day.
+
+**skyNightColor** (`tes3vector3`_, read-only)
+    The weather's sky color during the night.
+
+**skySunriseColor** (`tes3vector3`_, read-only)
+    The weather's sky color during sunrise.
+
+**skySunsetColor** (`tes3vector3`_, read-only)
+    The weather's sky color during sunset.
+
+**soundAmbientLoops** (`boolean`_)
+    Determines if ambient sound loops.
+
+**sunDayColor** (`tes3vector3`_, read-only)
+    The weather's sun color during the day.
+
+**sundiscSunsetColor** (`tes3vector3`_, read-only)
+    The weather's sun disc color.
+
+**sunNightColor** (`tes3vector3`_, read-only)
+    The weather's sun color during the night.
+
+**sunSunriseColor** (`tes3vector3`_, read-only)
+    The weather's sun color during sunrise.
+
+**sunSunsetColor** (`tes3vector3`_, read-only)
+    The weather's sun color during sunset.
+
+**transitionDelta** (`number`_)
+    Provides timing information for transitions between day, night, sunrise, and sunset.
+
+**underwaterSoundActive** (`boolean`_)
+    Determines if underwater sounds are active.
+
+**windSpeed** (`number`_)
+    The movement speed for the weather's wind.
+
+
+.. _`boolean`: ../lua/boolean.html
+.. _`number`: ../lua/number.html
+.. _`string`: ../lua/string.html
+.. _`table`: ../lua/table.html
+.. _`userdata`: ../lua/userdata.html
+
+.. _`tes3vector3`: vector3.html
