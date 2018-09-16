dc00e751567075531dfc97b7fe8750c313013b58
NullCascade <nullcascade@gmail.com>
2018-09-16 04:07:41 -0700

    Fix issue with previous commit.

--- misc/package/Data Files/MWSE/core/tes3/init.lua
+++ misc/package/Data Files/MWSE/core/tes3/init.lua
@@ -33,7 +33,14 @@ tes3.weaponType = require("tes3.weaponType")
 tes3.weather = require("tes3.weather")
 
 -- Second pass game constants, declared based on the above constants.
-tes3.magicSchoolSkill = require("tes3.magicSchoolSkill")
+tes3.magicSchoolSkill = {
+	[tes3.magicSchool.alteration] = tes3.skill.alteration,
+	[tes3.magicSchool.conjuration] = tes3.skill.conjuration,
+	[tes3.magicSchool.destruction] = tes3.skill.destruction,
+	[tes3.magicSchool.illusion] = tes3.skill.illusion,
+	[tes3.magicSchool.mysticism] = tes3.skill.mysticism,
+	[tes3.magicSchool.restoration] = tes3.skill.restoration,
+}
 
 -------------------------------------------------
 -- Functions
--- misc/package/Data Files/MWSE/core/tes3/magicSchoolSkill.lua
+++
@@ -1,8 +0,0 @@
-return {
-	[tes3.magicSchool.alteration] = tes3.skill.alteration,
-	[tes3.magicSchool.conjuration] = tes3.skill.conjuration,
-	[tes3.magicSchool.destruction] = tes3.skill.destruction,
-	[tes3.magicSchool.illusion] = tes3.skill.illusion,
-	[tes3.magicSchool.mysticism] = tes3.skill.mysticism,
-	[tes3.magicSchool.restoration] = tes3.skill.restoration,
-}
