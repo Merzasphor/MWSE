3b8958248566620644024e4e473c303f7f587a35
NullCascade <nullcascade@gmail.com>
2018-09-30 00:34:26 -0700

    Change the loaded event to send .mobile and .reference of the player.

--- MWSE/LuaLoadedGameEvent.cpp
+++ MWSE/LuaLoadedGameEvent.cpp
@@ -1,12 +1,58 @@
 #include "LuaLoadedGameEvent.h"
 
+#include "LuaManager.h"
+#include "LuaUtil.h"
+
+#include "TES3MobilePlayer.h"
+#include "TES3Reference.h"
+#include "TES3WorldController.h"
+
 namespace mwse {
 	namespace lua {
 		namespace event {
 			LoadedGameEvent::LoadedGameEvent(const char* fileName, bool quickLoad, bool newGame) :
-				LoadGameEvent(fileName, quickLoad, newGame)
+				GenericEvent("loaded"),
+				m_FileName(fileName),
+				m_QuickLoad(quickLoad),
+				m_NewGame(newGame)
 			{
-				m_EventName = "loaded";
+				if (m_FileName == NULL && !m_NewGame) {
+					m_QuickLoad = true;
+					m_FileName = "quiksave.ess";
+				}
+			}
+
+			sol::table LoadedGameEvent::createEventTable() {
+				sol::state& state = LuaManager::getInstance().getState();
+				sol::table eventData = state.create_table();
+
+				if (m_NewGame) {
+					eventData["newGame"] = true;
+					eventData["quickload"] = false;
+				}
+				else {
+					std::string filename = m_FileName;
+					eventData["filename"] = filename.substr(0, filename.find_last_of('.'));;
+					eventData["quickload"] = m_QuickLoad;
+					eventData["newGame"] = false;
+				}
+
+				auto mobilePlayer = tes3::getWorldController()->getMobilePlayer();
+				if (mobilePlayer) {
+					eventData["mobile"] = makeLuaObject(mobilePlayer);
+					eventData["reference"] = makeLuaObject(mobilePlayer->reference);
+				}
+
+				return eventData;
+			}
+
+			sol::object LoadedGameEvent::getEventOptions() {
+				sol::state& state = LuaManager::getInstance().getState();
+				sol::table options = state.create_table();
+
+				options["filter"] = m_FileName;
+
+				return options;
 			}
 		}
 	}
--- MWSE/LuaLoadedGameEvent.h
+++ MWSE/LuaLoadedGameEvent.h
@@ -1,13 +1,20 @@
 #pragma once
 
-#include "LuaLoadGameEvent.h"
+#include "LuaGenericEvent.h"
 
 namespace mwse {
 	namespace lua {
 		namespace event {
-			class LoadedGameEvent : public LoadGameEvent {
+			class LoadedGameEvent : public GenericEvent {
 			public:
 				LoadedGameEvent(const char* fileName, bool quickLoad = false, bool newGame = false);
+				sol::table createEventTable();
+				sol::object getEventOptions();
+
+			protected:
+				const char* m_FileName;
+				bool m_QuickLoad;
+				bool m_NewGame;
 			};
 		}
 	}
