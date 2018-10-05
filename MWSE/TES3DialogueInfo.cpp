84ac778e17c00f63faa0346ad6dddd5069af576b
NullCascade <nullcascade@gmail.com>
2018-10-05 16:05:49 -0700

    Remove in-dev feature related include that snuck in.

--- MWSE/TES3DialogueInfo.cpp
+++ MWSE/TES3DialogueInfo.cpp
@@ -2,16 +2,12 @@
 
 #include "TES3GameFile.h"
 
-#include "LuaInfoGetTextEvent.h"
-
 namespace TES3 {
 	const auto TES3_DialogueInfo_getText = reinterpret_cast<const char* (__thiscall*)(DialogueInfo*)>(0x4B1B80);
 	const auto TES3_DialogueInfo_loadId = reinterpret_cast<bool(__thiscall*)(DialogueInfo*)>(0x4B1A10);
 	const auto TES3_DialogueInfo_unloadId = reinterpret_cast<void(__thiscall*)(DialogueInfo*)>(0x4AF3A0);
 
 	const char* DialogueInfo::getText() {
-
-
 		return TES3_DialogueInfo_getText(this);
 	}
 
