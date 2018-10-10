202a2d8ae4c2e2e4f78bf840a39410c09ad10725
NullCascade <nullcascade@gmail.com>
2018-10-09 19:14:01 -0700

    Document NiProperty type constants.

--- MWSE/NIProperty.h
+++ MWSE/NIProperty.h
@@ -9,6 +9,21 @@ namespace NI {
 	};
 	static_assert(sizeof(Property_vTable) == 0x34, "NI::Property's vtable failed size validation");
 
+	enum class PropertyType : int {
+		Alpha = 0x0,
+		Fog = 0x1,
+		Material = 0x2,
+		Stencil = 0x3,
+		Texturing = 0x4,
+		VertexColor = 0x5,
+		Wireframe = 0x6,
+		ZBuffer = 0x7,
+		Dither = 0x8,
+		Specular = 0x9,
+		Shade = 0xA,
+		RendererSpecific = 0xB,
+	};
+
 	struct Property : ObjectNET {
 		unsigned short flags;
 
