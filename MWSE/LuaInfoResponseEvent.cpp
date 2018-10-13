741f6a7920241cdec4a657559bc8f18098deb6d3
NullCascade <nullcascade@gmail.com>
2018-10-13 12:56:54 -0700

    Add missing e.command event data to infoResponse event.

--- MWSE/LuaInfoResponseEvent.cpp
+++ MWSE/LuaInfoResponseEvent.cpp
@@ -13,6 +13,7 @@ namespace mwse {
 		namespace event {
 			InfoResponseEvent::InfoResponseEvent(const char* command, TES3::Reference * reference, TES3::ScriptVariables * variables, TES3::Dialogue * dialogue, TES3::DialogueInfo * info) :
 				ObjectFilteredEvent("infoResponse", info),
+				m_Command(command),
 				m_Reference(reference),
 				m_ScriptVariables(variables),
 				m_Dialogue(dialogue),
@@ -25,6 +26,7 @@ namespace mwse {
 				sol::state& state = LuaManager::getInstance().getState();
 				sol::table eventData = state.create_table();
 
+				eventData["command"] = m_Command;
 				eventData["reference"] = makeLuaObject(m_Reference);
 				eventData["variables"] = m_ScriptVariables;
 				eventData["dialogue"] = makeLuaObject(m_Dialogue);
