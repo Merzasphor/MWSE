843851efe5967cfbb4b394e244f526453a441077
NullCascade <nullcascade@gmail.com>
2018-10-19 20:40:01 -0700

    Remove infoFilter event. It never runs on the main thread so isn't terribly useful.

--- MWSE/TES3DialogueInfo.cpp
+++ MWSE/TES3DialogueInfo.cpp
@@ -2,13 +2,12 @@
 
 #include "LuaManager.h"
 
-#include "LuaInfoFilterEvent.h"
 
 namespace TES3 {
 	const auto TES3_DialogueInfo_getText = reinterpret_cast<const char* (__thiscall*)(DialogueInfo*)>(0x4B1B80);
 	const auto TES3_DialogueInfo_loadId = reinterpret_cast<bool(__thiscall*)(DialogueInfo*)>(0x4B1A10);
 	const auto TES3_DialogueInfo_unloadId = reinterpret_cast<void(__thiscall*)(DialogueInfo*)>(0x4AF3A0);
-	const auto TES3_DialogueInfo_filter = reinterpret_cast<bool(__thiscall*)(DialogueInfo*, Actor*, Reference*, signed char, Dialogue*)>(0x4B0190);
+	const auto TES3_DialogueInfo_filter = reinterpret_cast<bool(__thiscall*)(DialogueInfo*, Object*, Reference*, signed char, Dialogue*)>(0x4B0190);
 
 	const char* DialogueInfo::getText() {
 		return TES3_DialogueInfo_getText(this);
@@ -22,17 +21,8 @@ namespace TES3 {
 		TES3_DialogueInfo_unloadId(this);
 	}
 
-	bool DialogueInfo::filter(Actor * actor, Reference * reference, signed char source, Dialogue * dialogue) {
-		bool result = TES3_DialogueInfo_filter(this, actor, reference, source, dialogue);
-
-		sol::object eventResult = mwse::lua::LuaManager::getInstance().triggerEvent(new mwse::lua::event::InfoFilterEvent(this, actor, reference, source, dialogue, result));
-		if (eventResult.valid()) {
-			sol::table eventData = eventResult;
-			sol::optional<bool> passes = eventData["passes"];
-			result = passes.value_or(result);
-		}
-
-		return result;
+	bool DialogueInfo::filter(Object * actor, Reference * reference, signed char source, Dialogue * dialogue) {
+		return TES3_DialogueInfo_filter(this, actor, reference, source, dialogue);
 	}
 }
 
