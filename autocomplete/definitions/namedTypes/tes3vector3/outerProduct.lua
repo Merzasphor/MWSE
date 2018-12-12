8707ee45ec3e3a462276750861a8ddb5dc5d4195
Greatness7 <Greatness7@gmail.com>
2018-12-12 05:32:05 -0500

    Add missing autocomplete definitions for tes3vector3.

---
+++ autocomplete/definitions/namedTypes/tes3vector3/outerProduct.lua
@@ -0,0 +1,8 @@
+return {
+	type = "method",
+	description = [[Calculates the outer product with another vector.]],
+	arguments = {
+		{ name = "vec", type = "tes3vector3" },
+	},
+	valuetype = "tes3matrix3",
+}
