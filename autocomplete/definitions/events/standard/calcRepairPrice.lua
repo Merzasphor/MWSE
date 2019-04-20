de904545ce5b7934095d270b57a235517cc5aa4f
Pete <petethegoat@gmail.com>
2019-04-20 17:25:24 -0500

    More event definitions.

---
+++ autocomplete/definitions/events/standard/calcRepairPrice.lua
@@ -0,0 +1,32 @@
+return {
+	description = "This event is called when determining the price of repairing an item. It can be used to override the repair cost.",
+	eventData = {
+		mobile = {
+			type = "tes3mobileActor",
+			readonly = true,
+			description = "The mobile actor of the merchant the player is interacting with.",
+		},
+		reference = {
+			type = "tes3reference",
+			readonly = true,
+			description = "mobile’s related reference.",
+		},
+		basePrice = {
+			type = "number",
+			readonly = true,
+			description = "The price before adjustment.",
+		},
+		price = {
+			type = "number",
+			description = "The adjusted price of repairing item.",
+		},
+		item = {
+			type = "tes3item",
+			description = "The item to be repaired.",
+		},
+		itemData = {
+			type = "tes3item",
+			description = "Variable node. The data associated with item.",
+		},
+	},
+}
