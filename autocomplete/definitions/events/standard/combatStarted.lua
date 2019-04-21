cf3a45f8591cae305e6e1e34c93873f5749ca8b8
Pete <petethegoat@gmail.com>
2019-04-21 00:53:23 -0500

    More event definitions.

---
+++ autocomplete/definitions/events/standard/combatStarted.lua
@@ -0,0 +1,18 @@
+return {
+	description = "The combatStarted event occurs after combat has begun between two actors.",
+	eventData = {
+		actor = {
+			type = "tes3mobileActor",
+			readonly = true,
+			description = "The mobile actor who has entered combat.",
+		},
+		target = {
+			type = "tes3mobileActor",
+			readonly = true,
+			description = "The mobile actor who combat has been triggered against.",
+		},
+	},
+	links = {
+		["combatStart"] = "lua/event/combatStart",
+	},
+}
