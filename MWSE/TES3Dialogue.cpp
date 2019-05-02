bc1730a52238ee1c730103ab6862a642ff363a8b
NullCascade <nullcascade@gmail.com>
2019-05-01 23:26:46 -0700

    Minor cleanup of dialogue function calls.

--- MWSE/TES3Dialogue.cpp
+++ MWSE/TES3Dialogue.cpp
@@ -8,10 +8,6 @@
 #define TES3_Dialogue_journalSetIndex 0x50F8B0
 
 namespace TES3 {
-	const auto TES3_Dialogue_getFilteredInfo = reinterpret_cast<DialogueInfo* (__thiscall*)(Dialogue*, Actor*, Reference*, bool)>(0x4B29E0);
-
-	const auto TES3_getBaseDialogue = reinterpret_cast<Dialogue* (__cdecl*)(int, int)>(0x4B2C00);
-
 	bool Dialogue::addToJournal(int index, MobileActor * actor) {
 		if (type != DialogueType::Journal) {
 			return false;
@@ -59,11 +55,12 @@ namespace TES3 {
 	DialogueInfo* Dialogue::getDeepFilteredInfo(Actor* actor, Reference* reference, bool flag) {
 		auto info = getFilteredInfo(actor, reference, flag);
 		if (info == nullptr) {
-			info = TES3_getBaseDialogue(3, 0)->getFilteredInfo(actor, reference, flag);
+			info = getDialogue((int)DialogueType::Topic, 0)->getFilteredInfo(actor, reference, flag);
 		}
 		return info;
 	}
 
+	const auto TES3_Dialogue_getFilteredInfo = reinterpret_cast<DialogueInfo* (__thiscall*)(Dialogue*, Actor*, Reference*, bool)>(0x4B29E0);
 	DialogueInfo* Dialogue::getFilteredInfo(Actor* actor, Reference* reference, bool flag) {
 		return TES3_Dialogue_getFilteredInfo(this, actor, reference, flag);
 	}
