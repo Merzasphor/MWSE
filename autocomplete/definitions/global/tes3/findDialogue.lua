c8251ff10b40fca3c64012affe26c4a16109ad48
NullCascade <nullcascade@gmail.com>
2019-07-19 20:28:02 -0700

    Fix copy/paste error in tes3.findDialogue's definition.

--- autocomplete/definitions/global/tes3/findDialogue.lua
+++ autocomplete/definitions/global/tes3/findDialogue.lua
@@ -1,6 +1,6 @@
 return {
 	type = "function",
-	description = [[Drops one or more items from a reference's inventory onto the ground at their feet.]],
+	description = [[Locates a root dialogue that can then be filtered down for a specific actor to return a specific dialogue info For example, a type of 2 and a page of 1 will return the "Greeting 0" topic.]],
 	arguments = {{
 		name = "params",
 		type = "table",
