93b70693b060bfea56f847f841a6ba58481edf60
qqqbbb <qqqinbox@ya.ru>
2019-08-09 12:41:27 +0300

    actorType value description

--- autocomplete/definitions/namedTypes/tes3mobileActor/actorType.lua
+++ autocomplete/definitions/namedTypes/tes3mobileActor/actorType.lua
@@ -1,6 +1,6 @@
 return {
 	type = "value",
-	description = [[The type of the mobile actor, showing if it belongs to a creature, NPC, or the player.]],
+	description = [[The type of the mobile actor. 0 is a creature, 1 is an NPC, 2 is the player.]],
 	readOnly = true,
 	valuetype = "number",
 }
