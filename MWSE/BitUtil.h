9330c1e097895a366ee7c57ad0b43baa519eee0b
NullCascade <nullcascade@gmail.com>
2019-09-09 16:53:38 -0700

    Learn my left from my right.

--- MWSE/BitUtil.h
+++ MWSE/BitUtil.h
@@ -3,7 +3,7 @@
 #define BIT_SET_ON(d, p) d |= (1 << p)
 #define BIT_SET_OFF(d, p) d &= ~(1 << p)
 #define BIT_SET(d, p, v) v ? BIT_SET_ON(d, p) : BIT_SET_OFF(d, p)
-#define BIT_TEST(d, p) ((d << p) & 1) == 1
+#define BIT_TEST(d, p) ((d >> p) & 1) == 1
 
 #define BITMASK_SET_ON(d, m) d |= m
 #define BITMASK_SET_OFF(d, m) d &= ~m
