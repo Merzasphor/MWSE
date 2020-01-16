6baca046b30f33acfb853be13e0f2b02ce0eefb4
OperatorJack <thepaladinof@gmail.com>
2020-01-16 18:07:37 -0500

    Added documentation file for TES3Vector.interpolate.

---
+++ autocomplete/definitions/namedTypes/tes3vector3/interpolate.lua
@@ -0,0 +1,9 @@
+return {
+	type = "method",
+	description = [[Calculates the interpolated position against the target vector using the distance parameter.]],
+	arguments = {
+		{ name = "targetPoint", type = "tes3vector3" },
+		{ name = "distance", type = "float" },
+	},
+	valuetype = "tes3vector3",
+}
