5a0b37cbb50a2b5c9e029fa47ad320879d0df3f3
OperatorJack <thepaladinof@gmail.com>
2020-01-19 12:26:08 -0500

    Added documentation for isGuard and calcSunDamageScalar events.

---
+++ autocomplete/definitions/events/standard/calcSunDamageScalar.lua
@@ -0,0 +1,10 @@
+return {
+	description = "calcSunDamageScalar is used when calculating the amount of damage to apply for the Sun Damage magic effect.",
+	eventData = {
+		["sunDamage"] = {
+			type = "number",
+			readonly = false,
+			description = "The previously calculated sun damage.",
+		},
+	},
+}
