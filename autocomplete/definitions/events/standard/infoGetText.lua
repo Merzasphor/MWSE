c1396098c091fc5d59e1fe402e055932ba5c8eaf
OperatorJack <thepaladinof@gmail.com>
2020-02-12 20:13:40 -0500

    Added missing file contents to documents.

---
+++ autocomplete/definitions/events/standard/infoGetText.lua
@@ -0,0 +1,10 @@
+return {
+	description = "This event fires when a dialogue info object text is retrieved.",
+	eventData = {
+		["info"] = {
+			type = "tes3dialogueInfo",
+			readonly = true,
+			description = "The dialogue info object.",
+		},
+	},
+}
---
+++ autocomplete/definitions/events/standard/itemDropped.lua
@@ -0,0 +1,10 @@
+return {
+	description = "This event fires when an item is dropped.",
+	eventData = {
+		["reference"] = {
+			type = "tes3reference",
+			readonly = true,
+			description = "The reference.",
+		},
+	},
+}
