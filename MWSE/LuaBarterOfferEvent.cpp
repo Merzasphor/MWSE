9b83f98599981b5815770113f33edda5b134159b
Pete <petethegoat@gmail.com>
2020-04-23 18:52:30 -0500

    Added the "barterOffer" event- raised when a possibly successful barter is made. e.success based on whether it succeeded e.mobileActor is the bartering actor

---
+++ MWSE/LuaBarterOfferEvent.cpp
@@ -0,0 +1,34 @@
+#include "LuaBarterOfferEvent.h"
+
+#include "LuaManager.h"
+#include "LuaUtil.h"
+
+#include "TES3MobileActor.h"
+#include "TES3Reference.h"
+
+namespace mwse {
+	namespace lua {
+		namespace event {
+			BarterOfferEvent::BarterOfferEvent(TES3::MobileActor* mobileActor, bool success) :
+				GenericEvent("barterOffer"),
+				m_MobileActor(mobileActor),
+				m_Success(success)
+			{
+
+			}
+
+			sol::table BarterOfferEvent::createEventTable() {
+				auto stateHandle = LuaManager::getInstance().getThreadSafeStateHandle();
+				sol::state& state = stateHandle.state;
+				sol::table eventData = state.create_table();
+
+				eventData["mobileActor"] = makeLuaObject(m_MobileActor);
+				eventData["success"] = m_Success;
+
+				return eventData;
+			}
+
+			bool BarterOfferEvent::m_EventEnabled = false;
+		}
+	}
+}
