d6ec3e1aa28ab2978f5447b136bea724ac16d1a4
OperatorJack <thepaladinof@gmail.com>
2020-04-25 08:14:44 -0400

    Added tes3statisticlua to documentation.

---
+++ autocomplete/definitions/namedTypes/tes3statistic.lua
@@ -0,0 +1,4 @@
+return {
+	type = "class",
+	description = [[A structure that holds statistical information relating to the current and base values of a statistic such as health, fatigue, magicka, attributes, or skills.]],
+}
---
+++ autocomplete/definitions/namedTypes/tes3statistic/base.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The base value of the statistic. For example, for a mobile with a normal health amount of 100, fortified by 50, and having taken damage of 25, the base value would be 100, the normal health amount.]],
+	valuetype = "number",
+}
