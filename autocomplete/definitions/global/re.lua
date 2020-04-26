5e23feda3f8441da6dd15aef5d66cbdceaec10dd
Merzasphor <merzasphor@protonmail.com>
2020-04-26 14:22:56 -0400

    Add autocomplete docs for the re module.

---
+++ autocomplete/definitions/global/re.lua
@@ -0,0 +1,11 @@
+return {
+	type = "lib",
+	description = [[Provides a regular expression style syntax for pattern usage with lpeg.
+	
+The documentation provided here is only meant to be a quick reference. Please review the complete
+documentation (with in-depth discussion and many examples) at: 
+http://www.inf.puc-rio.br/~roberto/lpeg/re.html
+	
+There is also a copy in the MWSE repo at: deps/lpeg/re.html]],
+	link = "http://www.inf.puc-rio.br/~roberto/lpeg/re.html",
+}
