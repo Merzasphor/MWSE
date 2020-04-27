2827f11e71a15258834bc67d07b670012f03658c
OperatorJack <thepaladinof@gmail.com>
2020-04-27 17:00:48 -0400

    Documented NiPixelData lua bindings.

---
+++ autocomplete/definitions/namedTypes/netimmerse/niPixelData/bytesPerPixel.lua
@@ -0,0 +1,6 @@
+return {
+	type = "value",
+	description = [[The number of bytes per pixel.]],
+    valuetype = "number",
+    readOnly = true
+}
---
+++ autocomplete/definitions/namedTypes/netimmerse/niPixelData/getHeight.lua
@@ -0,0 +1,8 @@
+return {
+	type = "method",
+	description = [[Returns the height of the mipmap level at the given index, where level 1 is the finest (largest) mipmap level.]],
+	arguments = {
+		{ name = "mipMapLevel", type = "number", description = "Default value: 1", optional = true },
+	},
+	valuetype = "number",
+}
