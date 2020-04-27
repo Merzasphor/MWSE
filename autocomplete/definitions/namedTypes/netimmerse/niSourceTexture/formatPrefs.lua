468594e23507f255acc42ee8414fc6bd06d5afc8
OperatorJack <thepaladinof@gmail.com>
2020-04-27 19:16:17 -0400

    Documented NiSourceTexture and some related classes.

---
+++ autocomplete/definitions/namedTypes/netimmerse/niSourceTexture/formatPrefs.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The NiFormatPrefs of the texture.]],
+	valuetype = "niFormatPrefs",
+}
---
+++ autocomplete/definitions/namedTypes/netimmerse/niSourceTexture/pixelData.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The app-level source pixel data, if it exists (either because the texture was created from an NiPixelData object, or because createFromFilepath() has been called).]],
+	valuetype = "niPixelData",
+}
