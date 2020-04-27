eb1922613d1881a5e7809c159497461f5158bbcf
OperatorJack <thepaladinof@gmail.com>
2020-04-27 12:28:11 -0400

    Documented NiNode lua bindings.

---
+++ autocomplete/definitions/namedTypes/netimmerse/niNode/detachChild.lua
@@ -0,0 +1,8 @@
+return {
+	type = "method",
+	description = [[Detaches the child from the children list of the node. Returns the detached child.]],
+	arguments = {
+		{ name = "child", type = "niAVObject" },
+	},
+	valuetype = "niAVObject",
+}
