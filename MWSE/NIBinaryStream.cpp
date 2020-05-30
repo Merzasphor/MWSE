81399428481088cfc048ef523a0595a8074f7e97
NullCascade <nullcascade@gmail.com>
2020-05-29 22:17:08 -0700

    Fixup some binary stream vtable errors and other comments.

--- MWSE/NIBinaryStream.cpp
+++ MWSE/NIBinaryStream.cpp
@@ -2,10 +2,10 @@
 
 namespace NI {
     unsigned int BinaryStream::read(void* data, unsigned int bytes) {
-        return vTable.read(this, data, bytes);
+        return vTable->read(this, data, bytes);
     }
 
     unsigned int BinaryStream::write(const void* data, unsigned int bytes) {
-        return vTable.write(this, data, bytes);
+        return vTable->write(this, data, bytes);
     }
 }
