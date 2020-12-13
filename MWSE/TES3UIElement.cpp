96f8459011020263979501e94be4572413b10105
NullCascade <nullcascade@gmail.com>
2020-12-12 18:27:38 -0800

    Remove file existence check for tes3uiElement:createNif for now.

--- MWSE/TES3UIElement.cpp
+++ MWSE/TES3UIElement.cpp
@@ -1217,11 +1217,6 @@ namespace TES3 {
 			// Sanitize path.
 			std::replace(path.begin(), path.end(), '/', '\\');
 
-			// Make sure the file exists.
-			if (mwse::tes3::resolveAssetPath(path.c_str()) == 0) {
-				throw std::invalid_argument("Provided file does not exist.");
-			}
-
 			auto id = mwse::lua::getOptionalUIID(params, "id");
 			return createNif(id, path.c_str());
 		}
