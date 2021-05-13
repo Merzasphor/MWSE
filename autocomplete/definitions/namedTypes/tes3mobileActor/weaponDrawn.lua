3246da097ef141ddefa418c1b58fab3c602c67a2
Hrnchamd <iamalannoble@hotmail.com>
2021-05-13 23:15:06 +0100

    Clarify docs for tes3mobileActor.weaponDrawn vs tes3mobilePlayer.weaponReady.

--- autocomplete/definitions/namedTypes/tes3mobileActor/weaponDrawn.lua
+++ autocomplete/definitions/namedTypes/tes3mobileActor/weaponDrawn.lua
@@ -1,6 +1,6 @@
 return {
 	type = "value",
-	description = [[Friendly access to the actor's flag that controls if the actor has a weapon readied.]],
+	description = [[Friendly access to the actor's flag that shows if the weapon model is visible. When readying a weapon, there is a short period of time at the start of the animation, where the weapon is not visible yet. This flag will only be set after this initial stage is done. This flag is still set with hand-to-hand even though it doesn't use a model. Setting this to false while a weapon is drawn will normally cause the actor to play its weapon draw animation again.]],
 	readOnly = true,
 	valuetype = "boolean",
 }
--- autocomplete/definitions/namedTypes/tes3mobilePlayer/weaponReady.lua
+++ autocomplete/definitions/namedTypes/tes3mobilePlayer/weaponReady.lua
@@ -1,5 +1,5 @@
 return {
 	type = "value",
-	description = [[Toggle flag for if the player has a weapon ready.]],
+	description = [[Toggle flag for if the player has a weapon ready. Setting it to true will cause the player to take out their weapon. Setting it to false will cause them to put it away.]],
 	valuetype = "boolean",
 }
