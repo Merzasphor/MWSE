9e1c673e76e9f658531d902c2216c4ba5715c09b
Hrnchamd <iamalannoble@hotmail.com>
2021-05-16 19:59:08 +0100

    tes3ui.acquireTextInput docs.

--- autocomplete/definitions/global/tes3ui/acquireTextInput.lua
+++ autocomplete/definitions/global/tes3ui/acquireTextInput.lua
@@ -1,6 +1,8 @@
 return {
 	type = "function",
-	description = [[Sends all text input to the specified element. Suppresses keybinds while active. Calling this function with a nil argument will release text input and allow keybinds to work.]],
+	description = [[Sends all text input to the specified element.  Calling this function with a nil argument will release text input and allow keybinds to work. Suppresses most keybinds while active, except the Journal open/close keybind (it's up to the individual menu implementation).
+
+Only one element can have control of input, and there is no automatic restoration of control if one element takes control from another. Be careful of conflicts with other users of this function.]],
 	arguments = {
 		{ name = "element", type = "tes3uiElemenet", optional = true },
 	},
