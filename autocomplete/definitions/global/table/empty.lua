9a09d09076181496c6675cd627cf9ee395f5744e
C3pa <41503714+C3pa@users.noreply.github.com>
2021-06-06 22:54:02 +0200

    Add missing functions to table api docs (#309)
    * Add missing functions to table api docs
    
    * tabs

---
+++ autocomplete/definitions/global/table/empty.lua
@@ -0,0 +1,8 @@
+return {
+	type = "function",
+	description = [[Returns true if the table is empty, otherwise, it returns false.]],
+	arguments = {
+		{ name = "t", type = "table" },
+	},
+	valuetype = "boolean",
+}
