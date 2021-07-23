9b8ae3631bd4a306d1fa415ba85ce29ee433c3ee
Hrnchamd <iamalannoble@hotmail.com>
2021-07-23 21:21:58 +0100

    Docs update. Clarify equip usage.

--- autocomplete/definitions/events/standard/equip.lua
+++ autocomplete/definitions/events/standard/equip.lua
@@ -1,10 +1,13 @@
 return {
-	description = "The equip event fires when an actor is about to equip an item. This event allows scripts to block equipping.",
+	description = [[The equip event fires when an actor is about to equip an item, i.e. just before the item is equipped. This event allows scripts to block equipping. See `equipped`_ for the post-equip event.
+
+The item must not be removed from the inventory during this event, as the item and item data is still being referenced by the initiator of the equip action. If you want to consume the item, block the equip and remove it at a later time. Removing the item during this event can cause inventory desync bugs.]],
+	related = { "equip", "equipped", "unequipped" },
 	eventData = {
 		reference = {
 			type = "tes3reference",
 			readOnly = true,
-			description = "A reference to the mobile actor that is equipping the item.",
+			description = "The reference to the mobile actor that is equipping the item.",
 		},
 		item = {
 			type = "tes3baseObject",
@@ -17,5 +20,8 @@ return {
 			description = "The item data of item.",
 		},
 	},
-    	filter = "item",
+	filter = "item",
+	links = {
+		["equipped"] = "lua/event/equipped",
+	},
 }
