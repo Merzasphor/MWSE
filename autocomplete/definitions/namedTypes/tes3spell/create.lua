8deeedaf669bb50dad11389b1fff1e9df1c43906
C3pa <41503714+C3pa@users.noreply.github.com>
2021-07-23 09:37:06 +0200

    Change name argument for create (tes3spell) to optional (#323)

--- autocomplete/definitions/namedTypes/tes3spell/create.lua
+++ autocomplete/definitions/namedTypes/tes3spell/create.lua
@@ -1,8 +1,8 @@
 return {
 	type = "function",
 	description = [[Creates a new spell object, which will be stored as part of the current saved game.]],
-	arguments = {	
+	arguments = {
 		{ name = "id", type = "string", optional = false, description = "The new object's ID. Must be unique." },
-		{ name = "name", type = "string", optional = false, description = "The new object's name." },
+		{ name = "name", type = "string", optional = true, description = "The new object's name." },
 	}
 }
