215d84d42e89c957e0a074a96b53f405dab72231
NullCascade <nullcascade@gmail.com>
2021-07-24 15:09:08 -0700

    Fixups for latest doc work.

--- autocomplete/definitions/namedTypes/tes3repairTool.lua
+++ autocomplete/definitions/namedTypes/tes3repairTool.lua
@@ -1,5 +1,5 @@
 return {
 	type = "class",
 	description = [[A core repair tool object.]],
-	inherits = "tes3physicalObject",
+	inherits = "tes3item",
 }
--- autocomplete/definitions/namedTypes/tes3statisticSkill.lua
+++ autocomplete/definitions/namedTypes/tes3statisticSkill.lua
@@ -1,4 +1,7 @@
 return {
 	type = "class",
-	--description = [[]],
+	description = [[A structure that holds statistical information relating to the current and base values of a skill statistic.
+
+Morrowind statistics do not track buffs and debuffs separately. Instead, total buff or debuff values can be calculated with the tes3.getEffectMagnitude function, using the appropriate buff or debuff magic effect for the statistic.]],
+	inherits = "tes3statistic",
 }
