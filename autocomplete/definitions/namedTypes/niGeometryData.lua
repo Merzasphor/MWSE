87c38b67ff19ba520c5c31d6a8fab1e8a855d492
NullCascade <nullcascade@gmail.com>
2021-08-10 11:09:17 -0700

    Even more definition fixes.

--- autocomplete/definitions/namedTypes/niGeometryData.lua
+++ autocomplete/definitions/namedTypes/niGeometryData.lua
@@ -1,7 +1,5 @@
 return {
 	type = "class",
-	description = [[
-        NiGeometryData objects contain the geometry data necessary to render an NiGeometry object. When an NiGeometry-based object is created, the actual geometry data is stored in an attached NiGeometryData object. 
-    ]],
+	description = [[NiGeometryData objects contain the geometry data necessary to render an NiGeometry object. When an NiGeometry-based object is created, the actual geometry data is stored in an attached NiGeometryData object.]],
 	inherits = "niObject",
 }
