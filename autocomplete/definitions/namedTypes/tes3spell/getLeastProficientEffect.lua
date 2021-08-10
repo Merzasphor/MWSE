6950d8f4db7191e3c087b443b362b0f3f1606d37
NullCascade <nullcascade@gmail.com>
2021-08-10 11:07:52 -0700

    More definition fixes.

--- autocomplete/definitions/namedTypes/tes3spell/getLeastProficientEffect.lua
+++ autocomplete/definitions/namedTypes/tes3spell/getLeastProficientEffect.lua
@@ -1,5 +1,5 @@
 return {
-	type = "function",
+	type = "method",
 	description = [[Returns the effect of the spell that a given actor is least proficient with.]],
 	arguments = {	
 		{ name = "actor", type = "tes3mobileActor|tes3reference|tes3npc|tes3npcInstance", optional = false, description = "The actor to calculate for. A mobile actor is preferred, as passing only a tes3npc will not use up to date skill information." },
