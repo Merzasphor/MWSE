ca359807fe75825fca78671845a11a937b8d17c2
NullCascade <nullcascade@gmail.com>
2021-08-17 20:09:57 -0700

    God damn straggling files...

--- misc/package/Data Files/MWSE/core/meta/class/mwseLuaExecutor.lua
+++ misc/package/Data Files/MWSE/core/meta/class/mwseLuaExecutor.lua
@@ -3,8 +3,8 @@
 --- A handle to a command excuted using `os.execute` configured to run asynchronously.
 --- @class mwseLuaExecutor
 --- @field command string The command originally passed to `os.execute`.
---- @field errorCode string|nil The error code of the program, or 0 if there was no error.
---- @field exitCode string|nil The exit code of the program, or nil if the program has not yet finished.
+--- @field errorCode number The error code of the program, or 0 if there was no error.
+--- @field exitCode number|nil The exit code of the program, or nil if the program has not yet finished.
 --- @field ready boolean If true, the program is no longer running and output can be read.
 mwseLuaExecutor = {}
 
