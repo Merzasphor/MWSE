160b6c13eeb74a674c7c32af7f41d835200a7f31
NullCascade <nullcascade@gmail.com>
2021-10-17 07:05:05 -0700

    `tes3.showRestMenu`: Doc improvements. No need for weather checking. Whether checking is fine.

--- autocomplete/definitions/global/tes3/showRestMenu.lua
+++ autocomplete/definitions/global/tes3/showRestMenu.lua
@@ -7,10 +7,10 @@ Various parameters can be used to allow resting in situations not normally possi
 		name = "params",
 		type = "table",
 		tableParams = {
-			{ name = "checkForEnemies", type = "boolean", optional = true, default = true, description = "Perform a check weather there are enemies nearby before opening rest menu. If there are, false is returned." },
+			{ name = "checkForEnemies", type = "boolean", optional = true, default = true, description = "Perform a check whether there are enemies nearby before opening rest menu. If there are, false is returned." },
 			{ name = "checkForSolidGround", type = "boolean", optional = true, default = true, description = "Perform a check if the player is underwater. If underwater, false is returned." },
-			{ name = "checkSleepingIllegal", type = "boolean", optional = true, default = true, description = "Perform a check if the sleeping in the current cell is illegal. If illegal, false if returned." },
-			{ name = "checkIsWerewolf", type = "boolean", optional = true, default = true, description = "Perform a check if the player is Werewolf. If he/she is, then false is returned." },
+			{ name = "checkSleepingIllegal", type = "boolean", optional = true, default = true, description = "Perform a check if the sleeping in the current cell is illegal. If illegal, then the player will be prompted to wait instead of rest." },
+			{ name = "checkIsWerewolf", type = "boolean", optional = true, default = true, description = "Perform a check if the player is Werewolf. If they are, then the player will be prompted to wait instead of rest." },
 			{ name = "showMessage", type = "boolean", optional = true, default = true, description = "Should a messagebox be shown if the player can't open resting menu because some condition isn't met." },
 			{ name = "resting", type = "boolean", optional = true, default = true, description = "Should this be a rest?" },
 			{ name = "waiting", type = "boolean", optional = true, default = false, description = "Or, is this a wait?" },
