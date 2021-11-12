d3f576879c0c97ec2ad840d2aa287e027a366d6e
C3pa <ivan.cepanec1@gmail.com>
2021-11-12 17:07:49 +0100

    Attemped fix for changeEquipment's argument not visible

--- autocomplete/definitions/namedTypes/tes3combatSession/changeEquipment.lua
+++ autocomplete/definitions/namedTypes/tes3combatSession/changeEquipment.lua
@@ -1,6 +1,7 @@
 return {
 	type = "method",
-	description = "This method equips an item to the mobile in this combat sesion.",
-	arguments = { name = "equipmentStack", type = "tes3equipmentStack", description = "An item to equip." },
-
+	description = [[This method equips an item to the mobile in this combat sesion.]],
+	arguments = {
+		{ name = "equipmentStack", type = "tes3equipmentStack", description = "An item to equip." },
+	},
 }
