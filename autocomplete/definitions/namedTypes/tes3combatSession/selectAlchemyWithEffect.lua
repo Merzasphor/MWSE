f75b404a6b129eebbb0b5235bf684a75da6aedd3
C3pa <ivan.cepanec1@gmail.com>
2021-11-12 17:08:33 +0100

    Add arguments and return value for .selectAlchemyWithEffect()

--- autocomplete/definitions/namedTypes/tes3combatSession/selectAlchemyWithEffect.lua
+++ autocomplete/definitions/namedTypes/tes3combatSession/selectAlchemyWithEffect.lua
@@ -1,5 +1,10 @@
 return {
 	type = "method",
 	description = "Selects the alchemy item with the greatest value, for a given effect ID and loads it into the selectedAlchemy property.",
-	valuetype = "number",
+	arguments = { 
+		{ name = "id", type = "number" },
+	},
+	returns = {
+		{ name = "result", type = "number" },
+	}
 }
