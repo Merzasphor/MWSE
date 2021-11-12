57845897c0e9da68d6bbf2af9e4685ed8d5ec3f3
C3pa <ivan.cepanec1@gmail.com>
2021-11-12 17:08:59 +0100

    Change description of math library

--- autocomplete/definitions/global/math.lua
+++ autocomplete/definitions/global/math.lua
@@ -1,5 +1,5 @@
 return {
 	type = "lib",
-	description = "This library is an interface to the standard C math library. It provides all its functions inside the table math.",
+	description = "This library is an interface to the standard C math library. This library has been further extended by MWSE. The functions implemented by MWSE are listed here. It provides all its functions inside the table math.",
 	link = "https://www.lua.org/manual/5.1/manual.html#5.6",
 }
