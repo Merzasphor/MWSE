e4bf397c9df51667cd98c629c17eb6ba2de66824
C3pa <41503714+C3pa@users.noreply.github.com>
2021-11-14 19:05:22 +0100

    Add actor:hasItemEquipped() method to the docs

---
+++ autocomplete/definitions/namedTypes/tes3actor/hasItemEquipped.lua
@@ -0,0 +1,15 @@
+return {
+	type = "method",
+	description = [[Checks if the actor has provided item equipped.]],
+	arguments = {{
+		name = "params",
+		type = "table",
+		tableParams = {
+			{ name = "item", type = "tes3item|string", description = "The item to perform a check for." },
+        	{ name = "itemData", optional = true, type = "tes3itemData" },
+		},
+	}},
+	returns = {
+		{ name = "result", type = "boolean" },
+	}
+}
