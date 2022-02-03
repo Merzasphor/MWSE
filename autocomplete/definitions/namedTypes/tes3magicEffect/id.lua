657b6e3e1b5273c694295f6544f5d3c646598b5d
Pierre Steeg <pierre@steeg.email>
2022-02-03 23:40:56 +0100

    Added missing .lua file for previous commit.

--- autocomplete/definitions/namedTypes/tes3magicEffect/id.lua
+++ autocomplete/definitions/namedTypes/tes3magicEffect/id.lua
@@ -1,6 +1,6 @@
 return {
 	type = "value",
-	description = [[The unique identifier for the magic effect. Maps to values in [`tes3.effect`](https://mwse.github.io/MWSE/references/magic-effects/). Values not defined in `tes3.effect` are also possible for magic effects added by mods that don't use [`tes3.claimSpellEffectId`](https://mwse.github.io/MWSE/apis/tes3/#tes3claimspelleffectid) to register their id.]],
+	description = [[The unique identifier for the magic effect. Maps to values in [`tes3.effect`](https://mwse.github.io/MWSE/references/magic-effects/). Values not defined in `tes3.effect` are also possible for magic effects added by mods that don't use [`tes3.claimSpellEffectId()`](https://mwse.github.io/MWSE/apis/tes3/#tes3claimspelleffectid) to register their id.]],
 	valuetype = "number",
 	readOnly = true,
 }
