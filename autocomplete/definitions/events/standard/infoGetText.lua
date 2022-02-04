2d8644276abf4fa9c48aabafaeca1d4ae919c54a
Pierre Steeg <pierre@steeg.email>
2022-02-04 20:46:54 +0100

    Added docs for missing parameter in event infoGetText

--- autocomplete/definitions/events/standard/infoGetText.lua
+++ autocomplete/definitions/events/standard/infoGetText.lua
@@ -3,6 +3,10 @@ return {
 	description = "This event fires when a dialogue info object text is retrieved.",
 	related = { "infoGetText", "infoLinkResolve", "infoResponse", "infoFilter", "postInfoResponse" },
 	eventData = {
+		["text"] = {
+			type = "string",
+			description = "The text to return. This can be modified. Does not contain the original text. If not set, the original text will be used instead.",
+		},
 		["info"] = {
 			type = "tes3dialogueInfo",
 			readOnly = true,
