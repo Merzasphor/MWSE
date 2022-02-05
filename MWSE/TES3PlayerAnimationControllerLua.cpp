5c45bfaa63ee0f83351f8115de3c1f735d672735
Greatness7 <Greatness7@gmail.com>
2022-02-05 12:58:13 -0500

    Add property bindings for `tes3playerAnimationController`.

--- MWSE/TES3PlayerAnimationControllerLua.cpp
+++ MWSE/TES3PlayerAnimationControllerLua.cpp
@@ -3,6 +3,8 @@
 #include "LuaUtil.h"
 #include "LuaManager.h"
 
+#include "NICamera.h"
+
 #include "TES3PlayerAnimationController.h"
 
 namespace mwse::lua {
@@ -14,6 +16,20 @@ namespace mwse::lua {
 		// Start our usertype.
 		auto usertypeDefinition = state.new_usertype<TES3::PlayerAnimationController>("tes3playerAnimationController");
 		usertypeDefinition["new"] = sol::no_constructor;
+
+		// Basic property binding.
+		usertypeDefinition["firstPersonHeadCameraNode"] = sol::readonly_property(&TES3::PlayerAnimationController::firstPersonHeadCameraNode);
+		usertypeDefinition["cameraOffset"] = sol::readonly_property(&TES3::PlayerAnimationController::cameraOffset);
+		usertypeDefinition["pickData"] = sol::readonly_property(&TES3::PlayerAnimationController::pickData);
+		usertypeDefinition["is3rdPerson"] = sol::readonly_property(&TES3::PlayerAnimationController::is3rdPerson);
+		usertypeDefinition["togglePOV"] = sol::readonly_property(&TES3::PlayerAnimationController::togglePOV);
+		usertypeDefinition["allowVerticalAirControl"] = sol::readonly_property(&TES3::PlayerAnimationController::allowVerticalAirControl);
+		usertypeDefinition["vanityCameraEnabled"] = sol::readonly_property(&TES3::PlayerAnimationController::vanityCameraEnabled);
+		usertypeDefinition["useThirdPersonAfterVanityCameraDone"] = sol::readonly_property(&TES3::PlayerAnimationController::useThirdPersonAfterVanityCameraDone);
+		usertypeDefinition["vanityCameraZ"] = sol::readonly_property(&TES3::PlayerAnimationController::vanityCameraZ);
+		usertypeDefinition["vanityCameraDistance"] = sol::readonly_property(&TES3::PlayerAnimationController::vanityCameraDistance);
+		usertypeDefinition["vanityCamera"] = sol::readonly_property(&TES3::PlayerAnimationController::vanityCamera);
+		usertypeDefinition["shadowCameraMatrix"] = sol::readonly_property(&TES3::PlayerAnimationController::shadowCameraMatrix);
 
 		// Define inheritance structures. These must be defined in order from top to bottom. The complete chain must be defined.
 		usertypeDefinition[sol::base_classes] = sol::bases<TES3::ActorAnimationController>();
