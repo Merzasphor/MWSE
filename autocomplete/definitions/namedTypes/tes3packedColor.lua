01c4bed5e9c01e97a93c6da4f8b5bd912d40f525
C3pa <41503714+C3pa@users.noreply.github.com>
2022-04-01 21:56:17 +0200

    Improve descriptions for some definitions in the documentation

--- autocomplete/definitions/namedTypes/tes3packedColor.lua
+++ autocomplete/definitions/namedTypes/tes3packedColor.lua
@@ -1,4 +1,4 @@
 return {
 	type = "class",
-	description = [[A densely packed structure for single-byte red, green, blue and alpha values.]],
+	description = [[A densely packed structure for single-byte red, green, blue and alpha values. The values are from 0 to 255.]],
 }
