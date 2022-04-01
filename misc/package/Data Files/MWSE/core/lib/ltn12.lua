89727b4420374f58be5a0e8899a9ba50fecf74d3
C3pa <41503714+C3pa@users.noreply.github.com>
2022-04-01 21:21:27 +0200

    Silence the warnings in ltn12.lua

--- misc/package/Data Files/MWSE/core/lib/ltn12.lua
+++ misc/package/Data Files/MWSE/core/lib/ltn12.lua
@@ -9,12 +9,14 @@
 -----------------------------------------------------------------------------
 local string = require("string")
 local table = require("table")
-local unpack = unpack or table.unpack
+local unpack = unpack or table.unpack	---@diagnostic disable-line:deprecated
 local base = _G
 local select = select
 
 local _M = {}
+---@diagnostic disable-next-line:deprecated
 if module then -- heuristic for exporting a global package table
+	---@diagnostic disable-next-line:lowercase-global
     ltn12 = _M  -- luacheck: ignore
 end
 local filter,source,sink,pump = {},{},{},{}
