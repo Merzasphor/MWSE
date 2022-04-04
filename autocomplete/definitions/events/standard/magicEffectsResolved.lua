23bfd9d3c8c50e651e1f704ca82eac4db8040b84
Pierre Steeg <33100859+pierresteeg@users.noreply.github.com>
2022-04-04 19:47:42 +0200

    Fixed typos in magicEffectsResolved event docs

--- autocomplete/definitions/events/standard/magicEffectsResolved.lua
+++ autocomplete/definitions/events/standard/magicEffectsResolved.lua
@@ -1,4 +1,4 @@
 return {
 	type = "event",
-	description = "This event triggers after all magic effect links have been resolved. This event is used to add new spell effect by using [`tes3.addSpellEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) function. For examples of this event in practice see [`tes3.addMagicEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) example.",
+	description = "This event triggers after all magic effect links have been resolved. This event is used to add new magic effects by using the [`tes3.addMagicEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) function. For examples of this event in practice see the [`tes3.addMagicEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) example.",
 }
--- misc/package/Data Files/MWSE/core/meta/event/magicEffectsResolved.lua
+++ misc/package/Data Files/MWSE/core/meta/event/magicEffectsResolved.lua
@@ -1,6 +1,6 @@
 --- @meta
 --- @diagnostic disable:undefined-doc-name
 
---- This event triggers after all magic effect links have been resolved. This event is used to add new spell effect by using [`tes3.addSpellEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) function. For examples of this event in practice see [`tes3.addMagicEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) example.
+--- This event triggers after all magic effect links have been resolved. This event is used to add new magic effects by using the [`tes3.addMagicEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) function. For examples of this event in practice see the [`tes3.addMagicEffect()`](https://mwse.github.io/MWSE/apis/tes3/#tes3addmagiceffect) example.
 --- @class magicEffectsResolvedEventData
 --- @field claim boolean If set to `true`, any lower-priority event callbacks will be skipped. Returning `false` will set this to `true`.
