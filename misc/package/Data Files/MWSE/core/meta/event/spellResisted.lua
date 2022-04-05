e0b1c1095a4ef737fa1174c181ba537a0248c79c
Pierre Steeg <33100859+pierresteeg@users.noreply.github.com>
2022-04-06 01:11:39 +0200

    Added docs and examples for the "spellResisted" event

---
+++ misc/package/Data Files/MWSE/core/meta/event/spellResisted.lua
@@ -0,0 +1,16 @@
+--- @meta
+--- @diagnostic disable:undefined-doc-name
+
+--- This event triggers when a magic effect has been resisted by an actor, including magic effects from sources like spells, enchantments and potions. This event can trigger multiple times per source, once for each resisted effect.
+---
+--- [Examples available in online documentation](https://mwse.github.io/MWSE/events/spellResisted).
+--- @class spellResistedEventData
+--- @field claim boolean If set to `true`, any lower-priority event callbacks will be skipped. Returning `false` will set this to `true`.
+--- @field caster tes3reference *Read-only*. The caster of the magic source that contained the magic effect. Can be `nil`.
+--- @field effect tes3effect *Read-only*. The specific effect that triggered the event. This is equal to `e.source.effects[e.effectIndex]`.
+--- @field effectIndex number *Read-only*. The index of the effect in the magic source's effects list.
+--- @field mobile tes3mobileActor|tes3mobileCreature|tes3mobileNPC|tes3mobilePlayer *Read-only*. The mobile actor that resisted the magic effect.
+--- @field reference tes3reference *Read-only*. The reference of the mobile actor that resisted the magic effect.
+--- @field source tes3alchemy|tes3enchantment|tes3spell *Read-only*. The magic source that contains the effect.
+--- @field sourceInstance tes3magicSourceInstance *Read-only*. The unique instance of the magic source that contains the effect.
+--- @field target tes3reference *Read-only*. The target of the magic source that contained the magic effect. Can be `nil`.
