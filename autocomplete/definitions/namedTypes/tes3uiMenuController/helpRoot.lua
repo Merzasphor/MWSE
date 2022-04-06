ac7e5454bda3fc672537a656a46ee4c3dde5ef91
Pierre Steeg <33100859+pierresteeg@users.noreply.github.com>
2022-04-06 22:21:43 +0200

    Added docs for tes3uiMenuController.mainRoot/helpRoot #337

--- autocomplete/definitions/namedTypes/tes3uiMenuController/helpRoot.lua
+++ autocomplete/definitions/namedTypes/tes3uiMenuController/helpRoot.lua
@@ -1,6 +1,6 @@
 return {
 	type = "value",
 	readOnly = true,
-	description = [[]],
+	description = [[The root element of the help UI layer. The help UI layer will always be drawn after the main UI layer and therefore all elements on it will be shown in front of elements of the main UI layer. This element contains top level menus like tooltips, message boxes and more as direct children. Prefer using the [`tes3ui.findHelpLayerMenu()`](https://mwse.github.io/MWSE/apis/tes3ui/#tes3uifindhelplayermenu) function to find one of this element's direct children.]],
 	valuetype = "tes3uiElement",
 }
