fe9bbad8ecc0b36f5e5b288f884423285b33fd5a
NullCascade <nullcascade@gmail.com>
2022-04-21 14:24:17 -0700

    Update type annotations in docs for recent mkbuilder support changes.

--- autocomplete/definitions/namedTypes/tes3archive/sizesAndOffsets.lua
+++ autocomplete/definitions/namedTypes/tes3archive/sizesAndOffsets.lua
@@ -2,5 +2,5 @@ return {
 	type = "value",
 	description = [[An array of sizes and offsets for entries in the archive.]],
 	readOnly = true,
-	valuetype = "table<number, tes3archiveOffsetSizeData>",
+	valuetype = "tes3archiveOffsetSizeData[]",
 }
