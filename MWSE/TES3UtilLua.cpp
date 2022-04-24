c513c798cdb0937c285c6f3f7fec7dd59698b380
Pierre Steeg <pierre@steeg.email>
2022-04-25 01:04:25 +0200

    Possible fix for `tes3.transferItem` crashing when equipped items have scripts
    Could not reproduce this whatsoever and not really sure why this would fix it, but it has fixed it for Vitruvian_Guar.
    For some reason it had issues dereferencing the second itemDataRef pointer at line 3572 according to the crash dump.
    Did not encounter any other issues on my end.

--- MWSE/TES3UtilLua.cpp
+++ MWSE/TES3UtilLua.cpp
@@ -3542,19 +3542,19 @@ namespace mwse::lua {
 
 				// Then transfer over items with data.
 				while (itemsLeftToTransfer > 0) {
-					TES3::ItemData** itemDataRef = nullptr;
+					TES3::ItemData* itemDataRef = nullptr;
 
 					if (fromStack->variables) {
 						// We need to unequip the item first.
 						auto removedEquipStack = fromActor->unequipItem(item, false, fromMobile, false, fromStack->variables->at(0));
 						if (removedEquipStack == nullptr) {
 							// If nothing was returned then the item wasn't equipped. So transfer using the first data.
-							itemDataRef = &fromStack->variables->at(0);
+							itemDataRef = fromStack->variables->at(0);
 						}
 						else {
 							// Item was unequipped, but remains the first item? Preserve the item data.
 							if (fromStack->variables && fromStack->variables->at(0) == removedEquipStack->itemData) {
-								itemDataRef = &fromStack->variables->at(0);
+								itemDataRef = fromStack->variables->at(0);
 							}
 
 							// Clean up after our check and manually delete.
@@ -3565,12 +3565,12 @@ namespace mwse::lua {
 						}
 					}
 
-					toActor->inventory.addItem(toMobile, item, 1, false, itemDataRef);
-					fromActor->inventory.removeItemWithData(fromMobile, item, itemDataRef ? *itemDataRef : nullptr, 1, false);
+					toActor->inventory.addItem(toMobile, item, 1, false, &itemDataRef);
+					fromActor->inventory.removeItemWithData(fromMobile, item, itemDataRef, 1, false);
 
 					// Set this item's OnPCAdd notification script variable if it exists.
-					if (onPCAdd && itemDataRef && *itemDataRef && (*itemDataRef)->script) {
-						(*itemDataRef)->setScriptShortValue("OnPCAdd", 1);
+					if (onPCAdd && itemDataRef != nullptr && itemDataRef->script) {
+						(itemDataRef)->setScriptShortValue("OnPCAdd", 1);
 					}
 
 					fulfilledCount++;
