00bd9cbff699bddc44d1cb2935182981df6d2cd1
NullCascade <nullcascade@gmail.com>
2022-04-30 00:42:49 -0700

    Add template for MWSE dialogue extension library. To be implemented.

---
+++ misc/package/Data Files/MWSE/core/MWSE Dialogue Extensions/main.lua
@@ -0,0 +1,72 @@
+local function onDialogueEnvironmentCreated(e)
+	-- Cache the environment variables outside the function for easier access.
+	-- Dialogue scripters shouldn't have to constantly pass these to the functions anyway.
+	local env = e.environment
+	local reference = env.reference
+	local dialogue = env.dialogue
+	local info = env.info
+
+	-- These are default environment functions available to dialogue scripters.
+	-- They should be designed in a way that is easy for them to be called.
+
+	function env.CloseDialogue()
+		error("Not yet implemented.")
+	end
+
+	function env.NPCFollowMe()
+		error("Not yet implemented.")
+	end
+
+	function env.NPCWaitHere()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenAlchemyMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenBarterMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenClassMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenCompanionInventory()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenEnchantMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenPersuasionMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenRaceMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenRepairMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenSpellmakingMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenSpellMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenTrainingMenu()
+		error("Not yet implemented.")
+	end
+
+	function env.OpenTravelMenu()
+		error("Not yet implemented.")
+	end
+end
+event.register(tes3.event.dialogueEnvironmentCreated, onDialogueEnvironmentCreated, { priority = 100 })
