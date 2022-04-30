b24ae2e9ff56eee972a8c79532e760a0560b9111
NullCascade <nullcascade@gmail.com>
2022-04-30 00:43:08 -0700

    Fix docs for `tes3worldController:applyEnchantEffect`.

--- autocomplete/definitions/namedTypes/tes3worldController/applyEnchantEffect.lua
+++ autocomplete/definitions/namedTypes/tes3worldController/applyEnchantEffect.lua
@@ -1,13 +1,9 @@
 return {
 	type = "function",
 	description = [[This function applies an enchantment's effects to a scene node.]],
-	arguments = {{
-		name = "params",
-		type = "table",
-		tableParams = {
-			{ name = "node", type = "niNode", description = "A scene node to which to apply the enchantment's effects." },
-			{ name = "enchantment", type = "tes3enchantment", description = "The enchantment's effects to apply." },
-		},
-	}},
+	arguments = {
+		{ name = "node", type = "niNode", description = "A scene node to which to apply the enchantment's effects." },
+		{ name = "enchantment", type = "tes3enchantment", description = "The enchantment's effects to apply." },
+	},
 	valuetype = "boolean",
 }
