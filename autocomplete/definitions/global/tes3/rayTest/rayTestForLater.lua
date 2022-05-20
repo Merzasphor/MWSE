bac91b6097e48aa106bbfef93d27c859d5dfddad
C3pa <41503714+C3pa@users.noreply.github.com>
2022-05-20 20:12:25 +0200

    I forgot these

--- autocomplete/definitions/global/tes3/rayTest/rayTestForLater.lua
+++ autocomplete/definitions/global/tes3/rayTest/rayTestForLater.lua
@@ -1,17 +1,21 @@
-local result = tes3.rayTest{ -- result can get invalidated
+local result = tes3.rayTest{ -- the result can get invalidated
 	position = tes3.getPlayerEyePosition(),
 	direction = tes3.getPlayerEyeVector(),
 	ignore = { tes3.player }
 }
 
-local ref
+local refHandle
 
 if result then
-	ref = tes3.makeSafeObjectHandle(result.reference)
+	refHandle = tes3.makeSafeObjectHandle(result.reference)
 end
 
--- Before using ref, now we can chack if it is valid
-if ref:valid() then
-	-- Now we can safely do something with ref
+local function myFunction()
+	-- Before using the reference, we need to check that it's still valid.
+	-- References get unloaded on cell changes etc.
+	if refHandle:valid() then
+		-- Now we can safely do something with our stored reference.
+		local reference = refHandle:getObject()
 
+	end
 end
