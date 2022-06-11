9d0864a77ba53d49fc4fb0d56445565a637057f3
NullCascade <nullcascade@gmail.com>
2022-06-11 02:50:52 -0700

    Add missing weather name definition.

---
+++ autocomplete/definitions/namedTypes/tes3weather/name.lua
@@ -0,0 +1,6 @@
+return {
+	type = "value",
+	description = [[The weather's user-friendly name, hardcoded in English to match the INI settings name. These values can be `"Ashstorm"`, `"Blight"`, `"Blizzard"`, `"Clear"`, `"Cloudy"`, `"Foggy"`, `"Overcast"`, `"Rain"`, `"Snow"`, or `"Thunderstorm"` as appropriate.]],
+	readOnly = true,
+	valuetype = "string",
+}
