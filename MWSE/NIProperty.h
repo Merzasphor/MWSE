a4090e9bd6eb738208359f3bf5525cd9113e2f54
Greatness7 <Greatness7@gmail.com>
2022-06-21 10:22:21 -0400

    Allow up to 8 decal maps.

--- MWSE/NIProperty.h
+++ MWSE/NIProperty.h
@@ -164,9 +164,11 @@ namespace NI {
 			DECAL_4,
 			DECAL_5,
 			DECAL_6,
+			DECAL_7,
+			DECAL_8,
 
 			DECAL_FIRST = DECAL_1,
-			DECAL_LAST = DECAL_6,
+			DECAL_LAST = DECAL_8,
 
 			MAP_FIRST = BASE,
 			MAP_LAST = DECAL_LAST,
