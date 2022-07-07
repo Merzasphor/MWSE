e4dac4faab21f252a0560af573f962b2e127252b
C3pa <41503714+C3pa@users.noreply.github.com>
2022-07-07 08:53:15 +0200

    Docs: tes3animationDataSequenceGroup

---
+++ autocomplete/definitions/namedTypes/tes3animationDataSequenceGroup.lua
@@ -0,0 +1,4 @@
+return {
+	type = "class",
+	description = [[A game object which contains niSequence objects for each animation body section.]],
+}
---
+++ autocomplete/definitions/namedTypes/tes3animationDataSequenceGroup/leftArm.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The niSequence object for the actor's left arm.]],
+	valuetype = "niSequence",
+}
