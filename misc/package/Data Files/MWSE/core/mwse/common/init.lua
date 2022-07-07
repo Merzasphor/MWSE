b25a40ac89601812bd8d6b144b29aa882bad3ae3
Pierre Steeg <pierre@steeg.email>
2022-07-07 11:37:29 +0200

    Added various common helper functions to deal with parameter validation for lua core mods.

--- misc/package/Data Files/MWSE/core/mwse/common/init.lua
+++ misc/package/Data Files/MWSE/core/mwse/common/init.lua
@@ -2,6 +2,86 @@ local common = {}
 
 local dynamicTextMT = {}
 
+--- Returns `trueValue` if `condition` is `true`, otherwise returns `falseValue`.
+---@param condition boolean
+---@param trueValue any
+---@param falseValue any
+---@return any
+function common.ternary(condition, trueValue, falseValue)
+	if condition then
+		return trueValue
+	end
+	return falseValue
+end
+
+--- Returns `true` if the value is or inherits from tes3reference.
+---@param value any
+---@return boolean
+function common.isReference(value)
+	return type(value) == "userdata" and value.objectType and value.objectType == tes3.objectType.reference
+end
+
+--- Returns `true` if the value is or inherits from tes3mobileActor.
+---@param value any
+---@return boolean
+function common.isMobileActor(value)
+	return type(value) == "userdata" and value.objectType and
+			(value.objectType == tes3.objectType.mobileActor or
+			value.objectType == tes3.objectType.mobileCreature or
+			value.objectType == tes3.objectType.mobileNPC or
+			value.objectType == tes3.objectType.mobilePlayer)
+end
+
+--- Returns `true` if the value is or inherits from tes3mobileActor.
+---@param value any
+---@return boolean
+function common.isActor(value)
+	return type(value) == "userdata" and value.objectType and
+			(value.objectType == tes3.objectType.container or
+			value.objectType == tes3.objectType.creature or
+			value.objectType == tes3.objectType.npc)
+end
+
+--- Gets the related `tes3reference` of a value. Simply returns the value if it already is a `tes3reference`. Returns `nil` if none has been found.
+---@param value any
+---@return tes3reference?
+function common.getRelatedReference(value)
+	if value then
+		if type(value) == "string" then
+			return tes3.getReference(value)
+		elseif common.isReference(value) then
+			return value
+		elseif common.isMobileActor(value) then
+			return value.reference
+		elseif common.isActor(value) then
+			return value.reference or value.mobile and value.mobile.reference
+		end
+	end
+	return nil
+end
+
+--- Gets the related `tes3mobileActor` of a value. Simply returns the value if it already is a `tes3mobileActor`. Returns `nil` if none has been found.
+---@param value any
+---@return tes3mobileActor?
+function common.getRelatedMobileActor(value)
+	if value then
+		if type(value) == "string" then
+			local reference = tes3.getReference(value)
+			if reference and common.isMobileActor(reference.mobile) then
+				return reference.mobile
+			end
+		elseif common.isReference(value) and common.isMobileActor(value.mobile) then
+			return value.mobile
+		elseif common.isActor(value) then
+			local mobile = value.mobile or value.reference and value.reference.mobile
+			if common.isMobileActor(mobile) then
+				return mobile
+			end
+		end
+	end
+	return nil
+end
+
 function common.resolveDynamicValue(value, ...)
 	if (type(value) == "function") then
 		return value(...)
