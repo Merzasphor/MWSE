511d98ea6b6010c56d2f90f540e0653afc3e0dd2
Pierre Steeg <pierre@steeg.email>
2022-07-27 13:52:06 +0200

    Fixed MobileProjectile/MobileSpellProjectile vTables

--- MWSE/TES3MobileSpellProjectile.h
+++ MWSE/TES3MobileSpellProjectile.h
@@ -3,12 +3,12 @@
 #include "TES3MobileProjectile.h"
 
 namespace TES3 {
-	struct MobileSpellProjectile_vTable {
+	struct MobileSpellProjectile_vTable : MobileProjectile_vTable {
 		void* setMagicInstanceSerial; // 0xA4
 		void* setRotationSpeed; // 0xA8
 		void* launchProjectile; // 0xAC
 	};
-	static_assert(sizeof(MobileSpellProjectile_vTable) == 0xC, "TES3::MobileSpellProjectile_vTable failed size validation");
+	static_assert(sizeof(MobileSpellProjectile_vTable) == 0xB0, "TES3::MobileSpellProjectile_vTable failed size validation");
 
 	struct MobileSpellProjectile : MobileProjectile {
 		unsigned int spellInstanceSerial; // 0xAC
