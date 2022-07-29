6cb16ff2a6b4bb42a501c5d69abb8a1d1d2516a2
C3pa <41503714+C3pa@users.noreply.github.com>
2022-07-29 21:24:55 +0200

    Docs: advise using tes3.niType in niObject:isOfType and niObject:isInstanceOfType

--- autocomplete/definitions/namedTypes/niObject/isOfType.lua
+++ autocomplete/definitions/namedTypes/niObject/isOfType.lua
@@ -1,8 +1,8 @@
 return {
 	type = "method",
-	description = [[Determines if the object is of a given type. Types can be found in the tes3.niType table.]],
+	description = [[Determines if the object is of a given type. Types can be found in the [`tes3.niType`](https://mwse.github.io/MWSE/references/niTypes/) table.]],
 	arguments = {
-		{ name = "type", type = "number" },
+		{ name = "type", type = "number", description = "Use values in the [`tes3.niType`](https://mwse.github.io/MWSE/references/niTypes/) table." },
 	},
 	valuetype = "boolean",
 }
