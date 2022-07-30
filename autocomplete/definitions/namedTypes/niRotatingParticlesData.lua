a73049f67446ca70058dd83debb4d73f9cd41409
C3pa <41503714+C3pa@users.noreply.github.com>
2022-07-30 10:53:17 +0200

    Docs: add niRotatingParticlesData

---
+++ autocomplete/definitions/namedTypes/niRotatingParticlesData.lua
@@ -0,0 +1,5 @@
+return {
+	type = "class",
+	description = [[niRotatingParticlesData objects contain the geometry data necessary to render a niRotatingParticles object. When a niRotatingParticles-based object is created, the actual geometry data is stored in an attached niRotatingParticlesData object.]],
+	inherits = "niParticlesData",
+}
---
+++ autocomplete/definitions/namedTypes/niRotatingParticlesData/rotations.lua
@@ -0,0 +1,6 @@
+return {
+	type = "value",
+	description = [[The particle rotations. The length of the array is equal to `vertexCount`.]],
+	valuetype = "niQuaternion[]",
+	readOnly = true,
+}
