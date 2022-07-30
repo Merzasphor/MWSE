16c9ed7a93fd49e0ce3770d0d9e86beeae35a37f
C3pa <41503714+C3pa@users.noreply.github.com>
2022-07-30 19:07:21 +0200

    Docs: add niParticles

---
+++ autocomplete/definitions/namedTypes/niParticles.lua
@@ -0,0 +1,5 @@
+return {
+	type = "class",
+	description = [[An object that represent collections of large numbers of small point-like objects, designed for dynamic effects like fountains, waterfalls, snow, explosions, etc.]],
+	inherits = "niTriBasedGeometry",
+}
---
+++ autocomplete/definitions/namedTypes/niParticles/data.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The niParticlesData of the object. Manipulation should not be done directly. Instead, consider copying the data and assigning it to this shape.]],
+	valuetype = "niParticlesData",
+}
