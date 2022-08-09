fc4ea162139aa9d88699faece7f91bae70ae3668
C3pa <41503714+C3pa@users.noreply.github.com>
2022-08-09 23:33:20 +0200

    Docs: add niColorKey

---
+++ autocomplete/definitions/namedTypes/niColorKey.lua
@@ -0,0 +1,5 @@
+return {
+	type = "class",
+	description = [[A key with timing, and color in niColorData, which are found in niParticleColorModifiers.]],
+	inherits = "niAnimationKey",
+}
---
+++ autocomplete/definitions/namedTypes/niColorKey/color.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The color for this key.]],
+	valuetype = "niPackedColor",
+}
