00b10df74ca5437cf2536164089bd0be93a1f26c
C3pa <41503714+C3pa@users.noreply.github.com>
2022-08-11 20:17:40 +0200

    Docs: add niParticleColorModifer

---
+++ autocomplete/definitions/namedTypes/niParticleColorModifier.lua
@@ -0,0 +1,5 @@
+return {
+	type = "class",
+	description = [[This modifer alters the color of each particle as it ages.]],
+	inherits = "niParticleModifier",
+}
---
+++ autocomplete/definitions/namedTypes/niParticleColorModifier/colorData.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The parameters for this modifier.]],
+	valuetype = "niColorData",
+}
