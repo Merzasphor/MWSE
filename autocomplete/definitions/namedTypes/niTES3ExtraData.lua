1a4096b0d0a089221ccb630fdff275d6c1d9eab5
C3pa <41503714+C3pa@users.noreply.github.com>
2022-08-13 21:48:45 +0200

    Docs: add niTES3ExtraData

---
+++ autocomplete/definitions/namedTypes/niTES3ExtraData.lua
@@ -0,0 +1,5 @@
+return {
+	type = "class",
+	description = [[A simple extra data that stores a `tes3reference` object.]],
+	inherits = "niExtraData",
+}
---
+++ autocomplete/definitions/namedTypes/niTES3ExtraData/reference.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The reference stored on this extra data.]],
+	valuetype = "tes3reference",
+}
