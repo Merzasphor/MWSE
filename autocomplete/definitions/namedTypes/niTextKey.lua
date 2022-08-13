5a38f950c50051fda84eead3cb10df33c00bf169
C3pa <41503714+C3pa@users.noreply.github.com>
2022-08-13 21:56:44 +0200

    Docs: add niTextKey

---
+++ autocomplete/definitions/namedTypes/niTextKey.lua
@@ -0,0 +1,4 @@
+return {
+	type = "class",
+	description = [[A class used to represent text notes that have an associated time value. They are used for designating the start and stop of animations and the triggering of sounds.]],
+}
