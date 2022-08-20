2da30d197e5bc310e4431c3d0dadcd1bd20acc67
C3pa <41503714+C3pa@users.noreply.github.com>
2022-08-20 14:42:59 +0200

    Docs: add niCollisionGroup

---
+++ autocomplete/definitions/namedTypes/niCollisionGroup/colliders.lua
@@ -0,0 +1,6 @@
+return {
+	type = "value",
+	description = [[The array of collider objects.]],
+	readOnly = true,
+	valuetype = "niCollisionGroupRecord[]",
+}
---
+++ autocomplete/definitions/namedTypes/tes3mobManager/mobCollisionGroup.lua
@@ -0,0 +1,7 @@
+return {
+	type = "value",
+	description = [[Access to the collision group used by the game engine.]],
+	experimental = true,
+	readOnly = true,
+	valuetype = "niCollisionGroup",
+}
