28332d515a7b4c0690dde55b5812079bb21098cd
C3pa <41503714+C3pa@users.noreply.github.com>
2022-08-28 23:19:09 +0200

    Docs: update tes3scriptContext

--- autocomplete/definitions/namedTypes/tes3scriptContext/getVariableData.lua
+++ autocomplete/definitions/namedTypes/tes3scriptContext/getVariableData.lua
@@ -3,9 +3,9 @@ return {
 	description = [[This method fetches all of the script's variables as a table.]],
 	returns = {{
 		name = "results",
-		type = "table",
-		description = [[The table with all of script's variable names as keys. 
-		
+		type = "table<string, table>",
+		description = [[The table with all of script's variable names as keys.
+
 		Each entry has following fields:
 			1.) type - `s`, `f` or `l`, which stand for `short`, `float`, and `long`
 			2.) index - the order of occurance of the variable in the script, starting from `0`
--- misc/package/Data Files/MWSE/core/meta/class/tes3scriptContext.lua
+++ misc/package/Data Files/MWSE/core/meta/class/tes3scriptContext.lua
@@ -6,11 +6,12 @@
 
 --- A script context object, which allows variables to be get/set using the variable name.
 --- @class tes3scriptContext
+--- @operator len: number
 tes3scriptContext = {}
 
 --- This method fetches all of the script's variables as a table.
---- @return table results The table with all of script's variable names as keys. 
---- 		
+--- @return table<string, table> results The table with all of script's variable names as keys.
+--- 
 --- 		Each entry has following fields:
 --- 			1.) type - `s`, `f` or `l`, which stand for `short`, `float`, and `long`
 --- 			2.) index - the order of occurance of the variable in the script, starting from `0`
